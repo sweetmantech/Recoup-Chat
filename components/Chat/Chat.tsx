9b9f7edfbdb8042cd802a5d2428df73ec97fef36
SeniorDev <tech.eng.me@gmail.com>
2024-10-07 11:16:29 -0400

    I fix build error

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -10,7 +10,7 @@ const Chat = () => {
       className={`w-screen overflow-hidden ${messages.length ? "px-4 pt-20 mb-[150px] overflow-y-auto h-[calc(100vh-150px)]" : "flex flex-col items-center justify-center h-screen"}`}
     >
       <p className="font-sans font-semibold text-2xl mb-2 text-center">
-        How is Luh Tyler's 3D Game Performing?
+        {`How is Luh Tyler's 3D Game Performing?`}
       </p>
       <ChatInput />
       <Messages />
