dea93f630f3fd33ffdf5159c3f1a01acc8d9466d
SeniorDev <tech.eng.me@gmail.com>
2024-10-08 22:26:01 -0400

    update: suggestion fx.

--- components/Chat/Suggestions.tsx
+++ components/Chat/Suggestions.tsx
@@ -1,7 +1,11 @@
 import { SUGGESTIONS } from "@/lib/consts";
+import { useChatProvider } from "@/providers/ChatProvider";
 import { ArrowUpRightIcon } from "lucide-react";
+import { v4 as uuidV4 } from "uuid";
 
 const Suggestions = () => {
+  const { append } = useChatProvider();
+
   return (
     <div className="max-w-3xl mx-auto w-full px-2 mt-2 flex gap-3 flex-wrap">
       {SUGGESTIONS.map((suggestion: string) => (
@@ -9,6 +13,13 @@ const Suggestions = () => {
           key={suggestion}
           type="button"
           className="border border-gray-700 py-1 px-3 rounded-md flex gap-1 items-center text-sm"
+          onClick={() =>
+            append({
+              id: uuidV4(),
+              role: "user",
+              content: suggestion,
+            })
+          }
         >
           {suggestion}
           <ArrowUpRightIcon className="w-4 h-4" />
