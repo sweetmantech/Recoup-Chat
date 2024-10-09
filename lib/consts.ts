b36eddca5bd7a97739f88574f4dce65fd7604a95
Sweets Sweetman <sweetmantech@gmail.com>
2024-10-08 21:39:02 -0300

    I update stack point system ID to sidney's key.

--- lib/consts.ts
+++ lib/consts.ts
@@ -1,11 +1,11 @@
 // STACK EVENTS
 export const MESSAGE_SENT_EVENT = "message_sent";
 export const MESSAGE_SENT_POINT = 1;
-export const CHAT_POINT_SYSTEM_ID = 4104;
+export const CHAT_POINT_SYSTEM_ID = 4172;
 
 // Vercel AI SDK
 export const AI_MODEL = "gpt-4o-mini";
 
-export const TITLE = 'Luh Tyler Chat';
-export const DESCRIPTION = 'Chat with Recoupable. Generate insights with simple text prompts.';
-
+export const TITLE = "Luh Tyler Chat";
+export const DESCRIPTION =
+  "Chat with Recoupable. Generate insights with simple text prompts.";
