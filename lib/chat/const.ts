eed87376eee811df45b1aac826fb5163a312403c
SeniorDev <tech.eng.me@gmail.com>
2024-10-10 15:39:26 -0400

    update: context.

--- lib/chat/const.ts
+++ lib/chat/const.ts
@@ -12,9 +12,10 @@ Some example questions Willie might ask:
 6. How many spotify follows have we received in the past week?
 7. How many fans do I have?
 
-Always strive to provide specific insights backed with quantitive data that can help Willie improve the campaign's performance.`;
 
-export const HABIT_INSTRUCTION = `You are an AI assistant specializing in music marketing analytics. Your primary user is Willie, a marketing manager at Atlantic Records. Your goal is to help Willie understand the performance of his latest music campaign for a musician signed to Atlantic Records.
+Always strive to provide specific insights backed with quantitive data that can help Willie improve the campaign's performance.
 
-You should provide a broad overview of listening habits based on provided fan data including username, artist name, country, city, and user type.
-Make your questions richer and more flexible by providing representative country names, usernames, artist names, user types, etc.`;
+In addition, 
+    If you receive question related with listening habit, You should provide a broad overview of listening habits based on provided fan data including username, artist name, country, city, and user type.
+    In that case, Make your answers richer and more flexible by providing representative country names, usernames, artist names, user types, etc.
+`;
--- lib/chat/getChatContext.tsx
+++ lib/chat/getChatContext.tsx
@@ -1,5 +1,5 @@
 import { getSupabaseServerAdminClient } from "@/packages/supabase/src/clients/server-admin-client";
-import { HABIT_INSTRUCTION, INSTRUCTION } from "./const";
+import { INSTRUCTION } from "./const";
 import getFollows from "./getFollows";
 import getTopScore from "./getTopScore";
 import getMostPlayed from "./getMostPlayed";
@@ -13,12 +13,12 @@ import getRecentScore from "./getRecentScore";
 import getScoresInPast24 from "./getScoresInPast24";
 import getFans from "./getFans";
 
-const getChatContext = async (isHabitQuestion: boolean) => {
+const getChatContext = async () => {
   const context = [];
   const client = getSupabaseServerAdminClient();
   const scores: SCORE_EVENT[] = await getUsersScore();
 
-  context.push(isHabitQuestion ? HABIT_INSTRUCTION : INSTRUCTION);
+  context.push(INSTRUCTION);
 
   context.push(
     `\n\n1. Fans for the latest campaign in the format (userNames, artistNames, country, city, user_type):\n\t`,
@@ -26,59 +26,54 @@ const getChatContext = async (isHabitQuestion: boolean) => {
   const fanContext = await getFans(client);
   context.push(fanContext);
 
-  if (!isHabitQuestion) {
-    const follows = await getFollows(client);
-    context.push(`\n2. Followers: ${follows}`);
+  const follows = await getFollows(client);
+  context.push(`\n2. Followers: ${follows}`);
 
-    let scoreContext = `\n3. Scores of fan ( please calculate a count for each username to indicate the number of times each player has played the game.) \n`;
-    scores.map((score: SCORE_EVENT) => {
-      scoreContext =
-        scoreContext +
-        `{ username: "${score.metadata.username}", timeToGetScore: ${score.metadata.time}, fanId: "${score.metadata.userId}", score: ${score.points}, playedAt: ${new Date(score.timestamp).toDateString()}}\n`;
-    });
-    context.push(scoreContext);
+  let scoreContext = `\n3. Scores of fan ( please calculate a count for each username to indicate the number of times each player has played the game.) \n`;
+  scores.map((score: SCORE_EVENT) => {
+    scoreContext =
+      scoreContext +
+      `{ username: "${score.metadata.username}", timeToGetScore: ${score.metadata.time}, fanId: "${score.metadata.userId}", score: ${score.points}, playedAt: ${new Date(score.timestamp).toDateString()}}\n`;
+  });
+  context.push(scoreContext);
 
-    const topScore = getTopScore(scores);
-    context.push(
-      `\n4. Highest scoring fan: ${topScore.metadata.username}(score is ${topScore.points})`,
-    );
+  const topScore = getTopScore(scores);
+  context.push(
+    `\n4. Highest scoring fan: ${topScore.metadata.username}(score is ${topScore.points})`,
+  );
 
-    const mostPlayedFan = getMostPlayed(scores);
-    context.push(`\n5. Most played fan: ${mostPlayedFan}`);
+  const mostPlayedFan = getMostPlayed(scores);
+  context.push(`\n5. Most played fan: ${mostPlayedFan}`);
 
-    const streamsCount = await getStreamsCount(client);
-    context.push(`\n6. Streams Count: ${streamsCount}`);
+  const streamsCount = await getStreamsCount(client);
+  context.push(`\n6. Streams Count: ${streamsCount}`);
 
-    const usersInPast7 = await getSpotifyFansPast7(
-      client,
-      7 * 24 * 60 * 60 * 1000,
-    );
-    const userRows = usersInPast7.map((user) => Object.values(user));
-    const usersContext = `\n\n7. Users signed in with spotify in the past 7 days in the format ( Name, Country, Game, Timestamp ):\n\t
-    ${userRows.length ? userRows.join("\n\t") : "There are no users signed in in the past 7 days."}`;
-    context.push(usersContext);
+  const usersInPast7 = await getSpotifyFansPast7(
+    client,
+    7 * 24 * 60 * 60 * 1000,
+  );
+  const userRows = usersInPast7.map((user) => Object.values(user));
+  const usersContext = `\n\n7. Users signed in with spotify in the past 7 days in the format ( Name, Country, Game, Timestamp ):\n\t
+  ${userRows.length ? userRows.join("\n\t") : "There are no users signed in in the past 7 days."}`;
+  context.push(usersContext);
 
-    const startedFansCount = await getStartedFans(client);
-    context.push(
-      `\n8. Users started signing in with spotify: ${startedFansCount}`,
-    );
+  const startedFansCount = await getStartedFans(client);
+  context.push(
+    `\n8. Users started signing in with spotify: ${startedFansCount}`,
+  );
 
-    const followersCount = await getFollowersInPast(
-      client,
-      24 * 60 * 60 * 1000,
-    );
-    context.push(`\n9. New followers in past 24hrs: ${followersCount}`);
+  const followersCount = await getFollowersInPast(client, 24 * 60 * 60 * 1000);
+  context.push(`\n9. New followers in past 24hrs: ${followersCount}`);
 
-    const recentScore = getRecentScore(scores);
-    context.push(
-      `\n10. Fan who has the most recent score: ${recentScore.metadata.username}`,
-    );
+  const recentScore = getRecentScore(scores);
+  context.push(
+    `\n10. Fan who has the most recent score: ${recentScore.metadata.username}`,
+  );
 
-    const scoresInPast24 = getScoresInPast24(scores);
-    context.push(
-      `\n11. Count of people scored in the past 24hrs: ${scoresInPast24}`,
-    );
-  }
+  const scoresInPast24 = getScoresInPast24(scores);
+  context.push(
+    `\n11. Count of people scored in the past 24hrs: ${scoresInPast24}`,
+  );
 
   return context.join("\n");
 };
