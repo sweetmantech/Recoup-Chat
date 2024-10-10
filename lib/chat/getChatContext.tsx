3a25cfe966c57dbbdb7bb370eec65ab2ba2b8ff2
SeniorDev <tech.eng.me@gmail.com>
2024-10-10 14:35:18 -0400

    I add stack score list

--- lib/chat/getChatContext.tsx
+++ lib/chat/getChatContext.tsx
@@ -37,45 +37,53 @@ const getChatContext = async () => {
     const follows = await getFollows(client);
     context.push(`\n2. Followers: ${follows}`);
 
+    let scoreContext = `\n3. Scores of fan: \n`;
+    scores.map((score: SCORE_EVENT) => {
+      scoreContext =
+        scoreContext +
+        `{ username: "${score.metadata.username}", timeToGetScore: ${score.metadata.time}, fanId: "${score.metadata.userId}", score: ${score.points}, playedAt: ${new Date(score.timestamp).toDateString()}}\n`;
+    });
+    context.push(scoreContext);
+
     const topScore = getTopScore(scores);
     context.push(
-      `\n3. Highest scoring fan: ${topScore.metadata.username}(score is ${topScore.points})`,
+      `\n4. Highest scoring fan: ${topScore.metadata.username}(score is ${topScore.points})`,
     );
 
     const mostPlayedFan = await getMostPlayed(client);
-    context.push(`\n4. Most played fan: ${mostPlayedFan}`);
+    context.push(`\n5. Most played fan: ${mostPlayedFan}`);
 
     const streamsCount = await getStreamsCount(client);
-    context.push(`\n5. Streams Count: ${streamsCount}`);
+    context.push(`\n6. Streams Count: ${streamsCount}`);
 
     const usersInPast7 = await getSpotifyFansPast7(
       client,
       7 * 24 * 60 * 60 * 1000,
     );
     const userRows = usersInPast7.map((user) => Object.values(user));
-    const usersContext = `\n\n6. Users signed in with spotify in the past 7 days in the format ( Name, Country, Game, Timestamp ):\n\t
+    const usersContext = `\n\n7. Users signed in with spotify in the past 7 days in the format ( Name, Country, Game, Timestamp ):\n\t
     ${userRows.length ? userRows.join("\n\t") : "There are no users signed in in the past 7 days."}`;
     context.push(usersContext);
 
     const startedFansCount = await getStartedFans(client);
     context.push(
-      `\n7. Users started signing in with spotify: ${startedFansCount}`,
+      `\n8. Users started signing in with spotify: ${startedFansCount}`,
     );
 
     const followersCount = await getFollowersInPast(
       client,
       24 * 60 * 60 * 1000,
     );
-    context.push(`\n8. New followers in past 24hrs: ${followersCount}`);
+    context.push(`\n9. New followers in past 24hrs: ${followersCount}`);
 
     const recentScore = getRecentScore(scores);
     context.push(
-      `\n9. Fan who has the most recent score: ${recentScore.metadata.username}`,
+      `\n10. Fan who has the most recent score: ${recentScore.metadata.username}`,
     );
 
     const scoresInPast24 = getScoresInPast24(scores);
     context.push(
-      `\n10. Count of people scored in the past 24hrs: ${scoresInPast24}`,
+      `\n11. Count of people scored in the past 24hrs: ${scoresInPast24}`,
     );
   }
 
