1b4c036c1395517d83d51af2cc8bf2b3dc68c4ba
SeniorDev <tech.eng.me@gmail.com>
2024-10-24 05:12:02 -0400

    I add streaming route.

--- app/api/tool_call/route.tsx
+++ app/api/tool_call/route.tsx
@@ -1,6 +1,7 @@
 import { AI_MODEL } from "@/lib/consts";
 import { NextRequest } from "next/server";
-import OpenAI from "openai";
+import { openai } from "@ai-sdk/openai";
+import { LanguageModelV1, streamText } from "ai";
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
@@ -8,60 +9,46 @@ export async function POST(req: NextRequest) {
   const question = body.question;
 
   try {
-    const openai = new OpenAI();
+    const systemMessage = `
+    Context: ${JSON.stringify(context)}
+    Question: ${question}
 
-    const response = await openai.chat.completions.create({
-      model: AI_MODEL,
-      messages: [
-        {
-          role: "user",
-          content: `Context: ${JSON.stringify(context)}
-          Question: ${question}
+    Analyze the provided context and answer the question comprehensively. Follow these guidelines:
 
-          Analyze the provided context and answer the question comprehensively. Follow these guidelines:
+    1. Specific Focus:
+      - If the question asks for a count (e.g., total fans, premium users), respond with just the number.
+      - If the question asks for only artists, albums, episodes, playlists, audio books, tracks, shows, respond with only their information.
+      - If the question asks for listening habits:
+        ** Overview:
+          a. Provide a broad summary of listening trends.
+          b. Include details on popular genres, artists, content types and segment.
+          c. Mention specific fan names, countries, cities and segment.
+        ** Content Breakdown:
+          a. Highlight popular playlists, albums, episodes, audiobooks, shows, and tracks.
+          b. Identify standout artists and their impact on the fanbase.
+        ** Engagement Metrics:
+          a. Report on key statistics like total fans, plays, or other relevant metrics.
+          b. Identify top performers or outliers in the data.
 
-          1. Listening Habits Overview:
-            - Provide a broad summary of listening trends.
-            - Include details on popular genres, artists, and content types.
-            - Mention specific fan names, countries, and cities when relevant.
+    2. Recommendations:
+      - Suggest 2-3 actionable strategies to improve engagement based on the data.
+      - Example: "To boost engagement, consider launching a personalized playlist campaign featuring top artists from each user's listening history."
 
-          2. Content Breakdown:
-            - Highlight popular playlists, albums, episodes, audiobooks, shows, and tracks.
-            - Identify standout artists and their impact on the fanbase.
+    3. Trends and Insights:
+      - Identify any emerging trends or unique insights from the data.
+      - Compare to broader industry trends if relevant.
 
-          3. Specific Focus:
-            - If the question asks for a count (e.g., total fans, premium users), respond with just the number.
+    Ensure your answer is data-driven, insightful, and provides clear value for understanding and acting on the fan base's behavior.
+    Respond with a plain text string. Do not include any markdown formatting, JSON structure, or special characters. Avoid greetings, closings, or any meta-commentary about the response format.`;
 
-          4. User Segmentation:
-            - Distinguish between premium and free users if applicable.
-
-          5. Engagement Metrics:
-            - Report on key statistics like total fans, plays, or other relevant metrics.
-            - Identify top performers or outliers in the data.
-
-          6. Recommendations:
-            - Suggest 2-3 actionable strategies to improve engagement based on the data.
-            - Example: "To boost engagement, consider launching a personalized playlist campaign featuring top artists from each user's listening history."
-
-          7. Trends and Insights:
-            - Identify any emerging trends or unique insights from the data.
-            - Compare to broader industry trends if relevant.
-
-          Ensure your answer is data-driven, insightful, and provides clear value for understanding and acting on the fan base's behavior.`,
-        },
-        {
-          role: "system",
-          content: `Respond with a plain text string. Do not include any markdown formatting, JSON structure, or special characters. Avoid greetings, closings, or any meta-commentary about the response format.`,
-        },
-      ],
+    const result = await streamText({
+      model: openai(AI_MODEL) as LanguageModelV1,
+      system: systemMessage,
+      maxTokens: 1111,
+      temperature: 0.7,
     });
 
-    const answer = response.choices[0].message!.content!.toString();
-
-    return Response.json({
-      message: "success",
-      answer,
-    });
+    return result.toDataStreamResponse();
   } catch (error) {
     console.error(error);
     const message = error instanceof Error ? error.message : "failed";
--- hooks/useToolCall.tsx
+++ hooks/useToolCall.tsx
@@ -1,45 +1,58 @@
-import { useChatProvider } from "@/providers/ChatProvider";
+// import { useChatProvider } from "@/providers/ChatProvider";
 import { Message } from "ai";
+import { useChat } from "ai/react";
 import { useEffect, useState } from "react";
 
 const useToolCall = (message: Message) => {
   const [loading, setLoading] = useState(false);
-  const { finalCallback } = useChatProvider();
-  const [answer, setAnswer] = useState("");
-  const { clearQuery } = useChatProvider();
-  const [isCalled, setIsCalled] = useState(false);
-
+  // const { finalCallback } = useChatProvider();
+  // const [answer, setAnswer] = useState("");
+  // const { clearQuery } = useChatProvider();
+  // const [isCalled, setIsCalled] = useState(false);
+  const isCalled = false;
+  const answer = "";
   const toolInvocations = [...(message.toolInvocations || [])];
   const toolInvocationResult = toolInvocations?.filter(
     (toolInvocation) => toolInvocation.state === "result",
   )?.[0];
 
+  const { handleSubmit, messages } = useChat({
+    api: "/api/tool_call",
+    body: {
+      question: toolInvocationResult.result?.question || "",
+      context: toolInvocationResult.result?.context || "",
+    },
+  });
+
+  console.log("ZIAD", messages);
+
   useEffect(() => {
     const init = async () => {
       setLoading(true);
-      let answer = "";
-      const question = toolInvocationResult.result?.question || "";
-      const context = toolInvocationResult.result?.context || "";
+      // let answer = "";
+      // const question = toolInvocationResult.result?.question || "";
+      // const context = toolInvocationResult.result?.context || "";
 
-      if (question && context) {
-        setIsCalled(true);
-        const response = await fetch(`/api/tool_call`, {
-          method: "POST",
-          body: JSON.stringify({
-            context,
-            question,
-          }),
-        });
-        const data = await response.json();
-        answer = data.answer;
-        await finalCallback({
-          role: "assistant",
-          content: answer,
-          id: "",
-        });
-        setAnswer(answer);
-      }
-      clearQuery();
+      handleSubmit();
+      // if (question && context) {
+      //   setIsCalled(true);
+      //   const response = await fetch(`/api/tool_call`, {
+      //     method: "POST",
+      //     body: JSON.stringify({
+      //       context,
+      //       question,
+      //     }),
+      //   });
+      //   const data = await response.json();
+      //   answer = data.answer;
+      //   await finalCallback({
+      //     role: "assistant",
+      //     content: answer,
+      //     id: "",
+      //   });
+      //   setAnswer(answer);
+      // }
+      // clearQuery();
       setLoading(false);
     };
 
