e6b41656bf11db303f7d93d46100437da711e6d7
SeniorDev <tech.eng.me@gmail.com>
2024-10-24 07:50:35 -0400

    I fix font size.

--- components/Chat/FanTable.tsx
+++ components/Chat/FanTable.tsx
@@ -5,16 +5,16 @@ const FanTable = ({ fans }: { fans: FAN_TYPE[] }) => {
     <div className="border-gray-700 border-[1px] rounded-md w-full p-2">
       <table className="w-full">
         <thead>
-          <th className="text-sm text-left p-1">Name</th>
-          <th className="text-sm text-left p-1">Coutry</th>
-          <th className="text-sm text-left p-1">City</th>
+          <th className="text-xs text-left p-1">Name</th>
+          <th className="text-xs text-left p-1">Coutry</th>
+          <th className="text-xs text-left p-1">City</th>
         </thead>
         <tbody>
           {fans.map((fan, index) => (
             <tr key={index}>
-              <td className="text-sm p-1">{fan.name}</td>
-              <td className="text-sm p-1">{fan.country}</td>
-              <td className="text-sm p-1">{fan.city}</td>
+              <td className="text-xs p-1">{fan.name}</td>
+              <td className="text-xs p-1">{fan.country}</td>
+              <td className="text-xs p-1">{fan.city}</td>
             </tr>
           ))}
           <tr>
