207ace91e72afbb012cdfb6b2b07afeb779b9f3a
SeniorDev <tech.eng.me@gmail.com>
2024-10-29 15:45:40 -0400

    I fix feedback.

--- lib/tools/createArtist.tsx
+++ lib/tools/createArtist.tsx
@@ -5,14 +5,12 @@ import { ArtistToolResponse } from "@/types/Tool";
 
 const createArtist = (question: string, email: string) =>
   tool({
-    description: `IMPORTANT: Always call this tool for ANY question related to creating artist.:
+    description: `IMPORTANT: Always call this tool for ANY question related to creating artist:
     Do NOT attempt to answer questions on these topics without calling this tool first.
 
     Example questions that MUST trigger this tool:
-    - "Create a new aritst"
-    - "I wanna create a new artist."
-
-    When in doubt, call this tool to ensure you have the most up-to-date and accurate information.`,
+    - "Create a new aritst."
+    - "I wanna create a new artist."`,
     parameters: z.object({
       artist_name: z.string().describe("The name of the artist to be created."),
     }),
