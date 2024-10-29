616a63dca868c7c06a561733b69e4395f4ff40ba
SeniorDev <tech.eng.me@gmail.com>
2024-10-29 15:57:20 -0400

    I fix read artist fuction.

--- lib/tools/readArtists.tsx
+++ lib/tools/readArtists.tsx
@@ -2,10 +2,17 @@ import { getSupabaseServerAdminClient } from "@/packages/supabase/src/clients/se
 
 const readArtists = async (userEmail: string) => {
   const client = getSupabaseServerAdminClient();
+  const { data: userData } = await client
+    .from("accounts")
+    .select("*")
+    .eq("email", userEmail)
+    .single();
+
   const { data: artists, error } = await client
     .from("artists")
     .select("*")
-    .eq("email", userEmail);
+    .in("id", userData.artistIds || []);
+
   if (error) throw error;
 
   return artists || [];
