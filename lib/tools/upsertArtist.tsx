f169bed8ebefda84037a9f16e43a9c9a47895046
SeniorDev <tech.eng.me@gmail.com>
2024-10-29 02:12:46 -0400

    I update upsertArtist.

--- lib/tools/upsertArtist.tsx
+++ lib/tools/upsertArtist.tsx
@@ -2,6 +2,28 @@ import { getSupabaseServerAdminClient } from "@/packages/supabase/src/clients/se
 
 const upsertArtist = async (artistName: string, userEmail: string) => {
   const client = getSupabaseServerAdminClient();
+  const { data: found, error } = await client
+    .from("artists")
+    .select("*")
+    .eq("name", artistName);
+  if (error) throw error;
+
+  if (found.length > 0) {
+    const { data, error: updateError } = await client
+      .from("artists")
+      .update({
+        name: artistName,
+        email: userEmail,
+        timestamp: Date.now(),
+      })
+      .eq("id", found[0].id)
+      .select("*")
+      .single();
+    if (updateError) throw updateError;
+
+    return data;
+  }
+
   const { data, error: insertError } = await client
     .from("artists")
     .insert({
