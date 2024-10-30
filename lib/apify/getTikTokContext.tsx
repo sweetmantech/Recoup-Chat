3b322318d8135578353e38680e0fc48d6815d285
SeniorDev <tech.eng.me@gmail.com>
2024-10-30 13:45:42 -0400

    I fix tiktok content.

--- lib/apify/getTikTokContext.tsx
+++ lib/apify/getTikTokContext.tsx
@@ -2,21 +2,21 @@
 const getTikTokContext = (trends: any) => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return trends.map((trend: any) => ({
-    username: trend?.authorMeta?.name,
-    nickname: trend?.authorMeta?.nickName,
-    fans: trend?.authorMeta?.fans,
-    heart: trend?.authorMeta?.heart,
-    description: trend?.text,
-    region: trend?.authorMeta?.region,
-    musicName: trend?.musicMeta?.musicName,
-    musicAuthor: trend?.musicMeta?.musicAuthor,
-    diggCount: trend?.diggCount,
-    shareCount: trend?.shareCount,
-    playCount: trend?.playCount,
-    collectCount: trend?.collectCount,
-    commentCount: trend?.commentCount,
-    pfp: trend?.authorMeta?.avatar,
-    profileUrl: trend?.authorMeta?.profileUrl,
+    username: trend?.authorMeta?.name || "",
+    nickname: trend?.authorMeta?.nickName || "",
+    fans: trend?.authorMeta?.fans || 0,
+    heart: trend?.authorMeta?.heart || 0,
+    description: trend?.text || "",
+    region: trend?.authorMeta?.region || "",
+    musicName: trend?.musicMeta?.musicName || "",
+    musicAuthor: trend?.musicMeta?.musicAuthor || "",
+    diggCount: trend?.diggCount || 0,
+    shareCount: trend?.shareCount || 0,
+    playCount: trend?.playCount || 0,
+    collectCount: trend?.collectCount || 0,
+    commentCount: trend?.commentCount || 0,
+    pfp: trend?.authorMeta?.avatar || "",
+    profileUrl: trend?.authorMeta?.profileUrl || "",
   }));
 };
 
