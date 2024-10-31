43bc1269e38ce819f57665e092cb9c83efdbe07c
SeniorDev <tech.eng.me@gmail.com>
2024-10-31 02:08:23 -0400

    I update param.

--- lib/tools/createArtist.tsx
+++ lib/tools/createArtist.tsx
@@ -6,15 +6,17 @@ import { ArtistToolResponse } from "@/types/Tool";
 const createArtist = (question: string, email: string) =>
   tool({
     description: `
-    IMPORTANT!!!: Always call this tool for ANY question related to creating artist:
-    NOTE!!!: This feature will always run when prompted to create an artist, even if you don't get an artist name.
+    IMPORTANT: Always call this tool for ANY question related to creating artist:
     Do NOT attempt to answer questions on these topics without calling this tool first!!!
 
     Example questions that MUST trigger this tool:
     - "Create a new aritst."
     - "I wanna create a new artist."`,
     parameters: z.object({
-      artist_name: z.string().describe("The name of the artist to be created."),
+      artist_name: z
+        .string()
+        .optional()
+        .describe("The name of the artist to be created."),
     }),
     execute: async ({ artist_name }) => {
       if (!artist_name)
