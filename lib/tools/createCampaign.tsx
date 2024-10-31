cb85a4d9ed157b7e93aee7d4e3f1ae31c487070d
SeniorDev <tech.eng.me@gmail.com>
2024-10-31 03:45:47 -0400

    I update tool desc.

--- lib/tools/createCampaign.tsx
+++ lib/tools/createCampaign.tsx
@@ -8,7 +8,7 @@ const createCampaign = (question: string, email: string) =>
   tool({
     description: `
     IMPORTANT: Always call this tool for ANY question related to creating campaign:
-    NOTE!!!: This feature will always run when prompted to create an campaign, even if you don't get an artist id.
+    NOTE!!!: This feature will always run when prompted to create an campaign, even if you don't get an artist or client id.
     Do NOT attempt to answer questions on these topics without calling this tool first!!!
 
     Example questions that MUST trigger this tool:
