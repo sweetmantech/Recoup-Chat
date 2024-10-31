3aa1514e68a4de572588240cb815905033124425
SeniorDev <tech.eng.me@gmail.com>
2024-10-31 12:53:40 -0400

    I fix feedback.

--- lib/tools/createCampaign.tsx
+++ lib/tools/createCampaign.tsx
@@ -7,7 +7,7 @@ import readArtists from "../supabase/readArtists";
 const createCampaign = (question: string, email: string) =>
   tool({
     description: `
-    IMPORTANT: Always call this tool for ANY question related to creating campaign:
+    IMPORTANT: Always call this tool for ANY question related to creating a campaign:
     NOTE!!!: This feature will always run when prompted to create an campaign, even if you don't get an artist id or campaign name.
     Do NOT attempt to answer questions on these topics without calling this tool first!!!
 
