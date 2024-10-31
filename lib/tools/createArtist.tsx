577b7f2f1c90769d69b560bd498ce9cf2ee48d41
SeniorDev <tech.eng.me@gmail.com>
2024-10-31 02:10:34 -0400

    I update instruction.

--- lib/tools/createArtist.tsx
+++ lib/tools/createArtist.tsx
@@ -7,6 +7,7 @@ const createArtist = (question: string, email: string) =>
   tool({
     description: `
     IMPORTANT: Always call this tool for ANY question related to creating artist:
+    NOTE!!!: This feature will always run when prompted to create an artist, even if you don't get an artist name.
     Do NOT attempt to answer questions on these topics without calling this tool first!!!
 
     Example questions that MUST trigger this tool:
