74ca78332547552603fcbaa1dd25256f71e5041c
SeniorDev <tech.eng.me@gmail.com>
2024-11-01 06:45:50 -0400

    I add create campaign event.

--- components/Tools/Insights.tsx
+++ components/Tools/Insights.tsx
@@ -1,9 +1,19 @@
+import { useChatProvider } from "@/providers/ChatProvider";
 import { useToolCallProvider } from "@/providers/ToolCallProvider";
 import { ArtistToolResponse } from "@/types/Tool";
+import { v4 as uuidV4 } from "uuid";
 
 const Insights = () => {
   const { toolName, context, loading } = useToolCallProvider();
+  const { append } = useChatProvider();
 
+  const handleSubmit = () => {
+    append({
+      id: uuidV4(),
+      content: `Create a new campaign.`,
+      role: "user",
+    });
+  };
   return (
     <div>
       {toolName === "getArtistAnalysis" &&
@@ -17,6 +27,7 @@ const Insights = () => {
             <button
               type="button"
               className="border-gray-700 border-[1px] px-3 py-1 rounded-full text-sm mt-2"
+              onClick={handleSubmit}
             >
               Create a new campaign
             </button>
