baa54bcdc7a0174ef45bc4c253faf07c6b30b743
SeniorDev <tech.eng.me@gmail.com>
2024-11-01 03:11:21 -0400

    I fix font size.

--- components/Chat/Thinking.tsx
+++ components/Chat/Thinking.tsx
@@ -4,14 +4,12 @@ import { LoaderCircle, TvMinimalPlay } from "lucide-react";
 
 const Thinking = () => {
   const { toolCall } = useChatProvider();
-  console.log("ZIAD HERE", toolCall);
-
   return (
     <div className="p-3 flex gap-2 w-full max-w-3xl mx-auto items-center pb-2">
       <div className="size-fit">
         <TvMinimalPlay className="h-6 w-6" />
       </div>
-      <p>{getThinkingStatus(toolCall)}</p>
+      <p className="text-sm">{getThinkingStatus(toolCall)}</p>
       <LoaderCircle className="h-4 w-4 animate-spin" />
     </div>
   );
--- lib/getThinkingStatus.tsx
+++ lib/getThinkingStatus.tsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const getThinkingStatus = (toolCall: any) => {
   if (toolCall?.toolName === "getArtistAnalysis" && toolCall?.args?.user_name) {
-    return `Searching for @${toolCall?.args?.userName || ""} videos on tiktok...`;
+    return `Searching for @${toolCall?.args?.user_name || ""} videos on tiktok...`;
   }
 
   return "is thinking...";
