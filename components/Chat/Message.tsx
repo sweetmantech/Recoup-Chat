52433356cfbfff1cd5e2d4543c98a2f2273d20d9
SeniorDev <tech.eng.me@gmail.com>
2024-11-06 05:19:48 -0500

    I use img

--- components/Chat/Message.tsx
+++ components/Chat/Message.tsx
@@ -2,16 +2,19 @@ import { Message as AIMessage } from "ai";
 import ToolContent from "../Tools/ToolContent";
 import { useToolCallProvider } from "@/providers/ToolCallProvider";
 import ToolFollowUp from "../Tools/ToolFollowUp";
-import Image from "next/image";
 
 const Message = ({ message }: { message: AIMessage }) => {
   const { context } = useToolCallProvider();
   return (
     <div className="p-3 rounded-lg flex w-full gap-2">
       {message.role === "assistant" && (
-        <div className="!w-[10px] !h-[10px] relative">
-          <Image src={"/logo-light.png"} layout="fill" alt="not found logo" />
-        </div>
+        // eslint-disable-next-line @next/next/no-img-element
+        <img
+          src={"/logo-light.png"}
+          width={15}
+          height={15}
+          alt="not found logo"
+        />
       )}
       <div className={`grow ${message.role === "user" && "flex justify-end"}`}>
         {context && <ToolContent />}
