ca9ca2f52d2174f7bb860d7740205a405b106218
SeniorDev <tech.eng.me@gmail.com>
2024-11-06 08:55:09 -0500

    I fix UI.

--- components/Tools/UpdateArtistInfo.tsx
+++ components/Tools/UpdateArtistInfo.tsx
@@ -1,22 +1,8 @@
 import { useArtistProvider } from "@/providers/ArtistProvider";
-import { useChatProvider } from "@/providers/ChatProvider";
-import { useToolCallProvider } from "@/providers/ToolCallProvider";
 import { ArtistRecord } from "@/types/Artist";
-import { v4 as uuidV4 } from "uuid";
 
 const UpdateArtistInfo = () => {
   const { artists, setSelectedArtist, setArtistActive } = useArtistProvider();
-  const { question } = useToolCallProvider();
-
-  const { append } = useChatProvider();
-
-  const handleSubmit = async () => {
-    append({
-      id: uuidV4(),
-      content: question,
-      role: "user",
-    });
-  };
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const handleSelectArtist = (e: any) => {
@@ -29,10 +15,10 @@ const UpdateArtistInfo = () => {
 
   return (
     <div className="w-full">
-      <p className="text-sm">Please provide the artist id and campaign name.</p>
+      <p className="text-sm">Please select the artist to update info.</p>
       <section className="pt-2 flex flex-col gap-2">
         <fieldset className="flex gap-2 items-center">
-          <p className="text-sm">Artist Id:</p>
+          <p className="text-sm">Artist:</p>
           <select
             className="!bg-transparent border-gray-700 border-[1px] rounded-md px-2 text-center text-sm"
             onChange={handleSelectArtist}
@@ -48,13 +34,6 @@ const UpdateArtistInfo = () => {
             ))}
           </select>
         </fieldset>
-        <button
-          type="button"
-          onClick={handleSubmit}
-          className="border-gray-700 border-[1px] px-3 py-1 rounded-full text-sm w-fit"
-        >
-          Update Artist Info.
-        </button>
       </section>
     </div>
   );
