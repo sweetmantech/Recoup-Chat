772b182837baccdfe940691d7af9cb02c3757493
SeniorDev <tech.eng.me@gmail.com>
2024-11-07 12:33:47 -0500

    I fix trim function.

--- lib/removeHTMLTags.tsx
+++ lib/removeHTMLTags.tsx
@@ -1,5 +1,10 @@
 function removeHtmlTags(str: string) {
-  return str.replace(/<[^>]*>/g, "");
+  return str
+    .replace(/<!--.*?-->/g, "")
+    .replace(/<script[^>]*>.*?<\/script>/g, "")
+    .replace(/<style[^>]*>.*?<\/style>/g, "")
+    .replace(/<[^>]+>|[<>/;]/g, "")
+    .trim();
 }
 
 export default removeHtmlTags;
