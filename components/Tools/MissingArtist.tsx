2319934cc751a795e9681806d802c4539a063306
SeniorDev <tech.eng.me@gmail.com>
2024-11-08 09:08:11 -0500

    WIP

--- components/Tools/MissingArtist.tsx
+++ components/Tools/MissingArtist.tsx
@@ -1,35 +1,18 @@
-import { useChatProvider } from "@/providers/ChatProvider";
-import { useState } from "react";
-import { v4 as uuidV4 } from "uuid";
+import { useArtistProvider } from "@/providers/ArtistProvider";
 
 const MissingArtist = ({ description }: { description: string }) => {
-  const [newArtistName, setNewArtistName] = useState("");
-  const { append } = useChatProvider();
-
-  const handleSubmit = async () => {
-    append({
-      id: uuidV4(),
-      content: `Create a new artist named ${newArtistName}.`,
-      role: "user",
-    });
-  };
+  const { setIsOpenSettingModal } = useArtistProvider();
 
   return (
     <div>
       <p className="text-sm">{description}</p>
       <fieldset className="pt-2 flex gap-2 items-center">
-        <input
-          type="text"
-          onChange={(e) => setNewArtistName(e.target.value)}
-          className="!bg-transparent border-gray-700 border-[1px] rounded-md !outline-none px-2 py-1 text-sm"
-          placeholder="Input artist name."
-        />
         <button
           type="button"
-          onClick={handleSubmit}
+          onClick={() => setIsOpenSettingModal(true)}
           className="border-gray-700 border-[1px] px-3 py-1 rounded-full text-sm"
         >
-          Submit
+          Create a artist.
         </button>
       </fieldset>
     </div>
