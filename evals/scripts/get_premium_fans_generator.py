44104be1d527b04024ea5fa5432363138d176199
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-11-15 16:35:41 -0800

    I update ci/cd

--- evals/scripts/get_premium_fans_generator.py
+++ evals/scripts/get_premium_fans_generator.py
@@ -3,7 +3,7 @@ import os
 import string
 from supabase import create_client
 
-supabase_url = os.environ.get("SUPABSE_URL")
+supabase_url = os.environ.get("SUPABASE_URL")
 supabase_key = os.environ.get("SUPABASE_KEY")
 supabase = create_client(supabase_url, supabase_key)
 
