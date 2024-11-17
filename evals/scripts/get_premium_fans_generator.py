4bd8289fc8bc5195b1199b71b6757b49332b58ca
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-11-17 13:18:07 -0800

    I fix error

--- evals/scripts/get_premium_fans_generator.py
+++ evals/scripts/get_premium_fans_generator.py
@@ -1,6 +1,7 @@
 import json
 import os
 from create_yaml_data import create_yaml, create_data
+from create_campaign_registry_data import create_campaign_registry_data
 
 create_yaml("get_premium_fans")   
 
