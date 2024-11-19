511cba5f1ec713f132830fb20d5c7ef52adaed1d
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-11-19 03:10:15 -0800

    I update instruction.

--- evals/scripts/campaign_evals_questions_ideals.py
+++ evals/scripts/campaign_evals_questions_ideals.py
@@ -35,7 +35,7 @@ evals_questions_ideals = [
         "question": "What are the top five playlists among users?",
         "ideal": "playlists",
         "eval_name": "get_top_five_playlists",
-        "criteria": "The answer is the list of playlist names like reference answer. Playlist names can also consist only of emoji unicode. So, please, don't ignore this case."
+        "criteria": "The answer is the playlists like reference answer. Playlist name can also consist only of emoji."
     },
     {
         "question": "How many total playlists are fans engaging with?",
