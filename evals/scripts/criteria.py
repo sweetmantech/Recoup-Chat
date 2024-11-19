a57fe024c69b9241a4435517b25a6a9c8c4005db
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-11-19 13:45:25 -0800

    I fix instruction.

--- evals/scripts/criteria.py
+++ evals/scripts/criteria.py
@@ -1,3 +1,3 @@
-count_query_criteria = "The answer should be numerically correct and include appropriate units (if applicable). The solution should match exactly with the reference answer."
+count_query_criteria = "The number of answer must be equal to the ideal value."
 format_match_criteria = "The answer should list of elements including items like ideal value"
 subset_criteria = "The actual answer should be a subset of the reference answer, answers can include emoji unicode values."
