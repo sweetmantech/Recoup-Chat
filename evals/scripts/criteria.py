2c792078e3ba9dde16012d6f3c2f640bb2997da6
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-11-19 01:45:44 -0800

    I update criteria

--- evals/scripts/criteria.py
+++ evals/scripts/criteria.py
@@ -1,3 +1,3 @@
 count_query_criteria = "The answer should be numerically correct and include appropriate units (if applicable). The solution should match exactly with the reference answer."
-sub_set_criteria = "The submission will be a subset of the reference answer, answers can include emoji unicode values."
+sub_set_criteria = "The submission should be a subset of the reference answer, answers can include emoji unicode values."
 format_match_criteria = "The actual answer should be formatted in the ideal value format."
