2a39e6f2e632dab8897264bdc1c645103fc440f1
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-11-18 22:51:16 -0800

    I fix style

--- evals/cli/oaieval.py
+++ evals/cli/oaieval.py
@@ -134,7 +134,7 @@ def send_slack_message(result: dict[str, Any], label) -> None:
                 message_text += " ✅\n" if value == 1.0 else " ❌\n"
             if "answer" in key:
                 answer = extract_between_markers(value[0], '[SUBMISSION]:', '1.')
-                message_text += f"• answer: {answer} {'✅' if 'Y' in key else '❌'}\n"
+                message_text += f"• {answer} {'✅' if 'Y' in key else '❌'}\n"
         
         response = client.chat_postMessage(
             channel=os.environ.get("SLACK_CHANNEL_ID", ""),
