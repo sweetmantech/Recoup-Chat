eac81f820e6f41a4aeca60ebba0916ca1c218b72
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-11-19 02:47:33 -0800

    I revert change

--- evals/scripts/criteria.py
+++ evals/scripts/criteria.py
@@ -1,3 +1,3 @@
 count_query_criteria = "The answer should be numerically correct and include appropriate units (if applicable). The solution should match exactly with the reference answer."
-sub_set_criteria = "Answer elements should appear in the reference answer in JSON array format."
+sub_set_criteria = "The submission should be a subset of the reference answer."
 format_match_criteria = "The actual answer should be formatted in the ideal value format."
