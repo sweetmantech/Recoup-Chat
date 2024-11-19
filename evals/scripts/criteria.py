1f7fb985293ada3cc51c0d13f089bcd78d2e05b3
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-11-19 13:56:48 -0800

    I fix criteria

--- evals/scripts/criteria.py
+++ evals/scripts/criteria.py
@@ -1,3 +1,3 @@
 count_query_criteria = "The number of answer must be equal to the ideal value."
 format_match_criteria = "The answer should list of elements including items like ideal value"
-subset_criteria = "The actual answer should be a subset of the reference answer, answers can include emoji unicode values."
+subset_criteria = "The expected answer should be considered correct if it is a subset of the reference answer, allowing for some items to be included from the list, regardless of order or completeness."
