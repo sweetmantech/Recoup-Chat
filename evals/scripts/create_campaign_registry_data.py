cceadf53b9224f4849e02c35be41e6e82dee29c7
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-11-19 19:09:30 -0800

    I fix instruction.

--- evals/scripts/create_campaign_registry_data.py
+++ evals/scripts/create_campaign_registry_data.py
@@ -11,21 +11,20 @@ def create_campaign_registry_data(ideal_key_or_value, question):
         context = get_context(artist_id, email)
         instruction = get_instruction()
 
+        if context:
+            context_str = json.dumps(context)
+        else:
+            context_str = "No context available."
+
         ideal_value = context.get(ideal_key_or_value, ideal_key_or_value)
         ideal_value_str = json.dumps(ideal_value) if isinstance(ideal_value, (dict, list)) else ideal_value
 
-        if question == "What are the most common podcast genres among fans?":
-            instruction_str = instruction['get_common_podcast_genres']
-            context_str = json.dumps(context.get('episodes_descriptions', []))
-        else:
-            instruction_str = instruction['get_campaign']
-            context_str = json.dumps(context)
-
         content = {
             "input": (
+                f"\n"
                 f"Context: {context_str}\n"
-                f"Question {question}\n"
-                f"{instruction_str}"
+                f"Question: {question}\n\n"
+                f"{instruction['get_campaign']}"
             ),
             "ideal": ideal_value_str,
         }
