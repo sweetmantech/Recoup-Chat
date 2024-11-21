1602c18f5eaf6f88e113111ad8bd74a1591aba39
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-11-21 02:27:38 -0800

    I update api

--- lib/apify/getDefaultDataset.tsx
+++ lib/apify/getDefaultDataset.tsx
@@ -1,16 +1,21 @@
 const getDefaultDataset = async (defaultDatasetId: string) => {
-  const response = await fetch(
-    `https://api.apify.com/v2/datasets/${defaultDatasetId}/items?token=${process.env.APIFY_TOKEN}`,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
+  try {
+    const response = await fetch(
+      `https://api.apify.com/v2/datasets/${defaultDatasetId}/items?token=${process.env.APIFY_TOKEN}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
       },
-    },
-  );
-  const data = await response.json();
+    );
+    const data = await response.json();
 
-  return data;
+    return data || [];
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 };
 
 export default getDefaultDataset;
