7bab1b10d7c0a2846f3278b8a67dafa838bd08ae
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-11-21 09:48:19 -0800

    I try new api

--- app/api/get_tiktok_account_trends/route.ts
+++ app/api/get_tiktok_account_trends/route.ts
@@ -1,3 +1,5 @@
+import getDefaultDataset from "@/lib/apify/getDefaultDataset";
+import getFormattedAccountInfo from "@/lib/apify/getFormattedAccountInfo";
 import runTikTokActor from "@/lib/apify/runTikTokActor";
 import { NextRequest } from "next/server";
 
@@ -12,11 +14,19 @@ export async function GET(req: NextRequest) {
   };
 
   try {
-    const data = await runTikTokActor(input, "clockworks~tiktok-scraper");
-    return Response.json({
-      success: true,
-      data,
-    });
+    const defaultDatasetId = await runTikTokActor(input, "clockworks~tiktok-scraper");
+    const timer = setInterval(async () => {
+      const data = await getDefaultDataset(defaultDatasetId as string);
+      const formattedData = getFormattedAccountInfo(data);
+
+      if (formattedData.length) {
+        clearInterval(timer)
+        return Response.json({
+          success: true,
+          data,
+        });
+      }
+    })
   } catch (error) {
     console.error(error);
     const message = error instanceof Error ? error.message : "failed";
