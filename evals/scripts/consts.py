5597cbd1d6f98d453d5a77de698b5fe84bded8b9
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-11-21 02:25:24 -0800

    WIP

--- evals/scripts/consts.py
+++ evals/scripts/consts.py
@@ -2,4 +2,4 @@ mock_data = [
     ('a4c897f5-02cb-43d3-b971-948c85537785', 'sidney@syncstream.ai'),
     ('a4c897f5-02cb-43d3-b971-948c85537785', 'sweetmantech@gmail.com'),
 ]
-api_endpoint = "https://recoup-chat-git-tech322-prod-match-recoupable-ad724970.vercel.app"
+api_endpoint = "https://recoup-chat-git-tech322-videocommentsapi-recoupable-ad724970.vercel.app/"
---
+++ evals/scripts/create_tiktok_vi_comments_registry_data.py
@@ -0,0 +1,57 @@
+import json
+import string
+import requests
+import urllib.parse
+import time
+from consts import api_endpoint
+from get_instruction import get_instruction
+
+def fetch_default_dataset():
+    encoded_postURLs = urllib.parse.quote("[\"https://www.tiktok.com/@bellapoarch/video/6862153058223197445\"]")
+    url = f"{api_endpoint}/api/get_tiktok_video_comments?postURLs={encoded_postURLs}"
+    response = requests.get(url)
+    data = response.json()
+    defaultDatasetId = data['data']
+
+    return defaultDatasetId
+
+def fetch_comments(defaultDatasetId):
+    response = requests.get(f"{api_endpoint}/api/get_tiktok_video_comments/get_dataset_items?defaultDatasetId={defaultDatasetId}")
+    data = response.json()
+    if len(data) > 0:
+        return data
+    else:
+        return None
+
+def create_tiktok_vi_comments_registry_data(ideal_key_or_value, question):
+    registry_data = ""
+    defaultDatasetId = fetch_default_dataset()
+    instruction = get_instruction()
+
+    while True:
+        data = fetch_comments(defaultDatasetId)
+        if data['data']:
+            context_str = json.dumps(data['data'])
+        else:
+            context_str = "No context available."
+        if data is not None:
+            
+            break
+        time.sleep(2)
+
+    ideal_value = data['data'].get(ideal_key_or_value, ideal_key_or_value)
+    ideal_value_str = json.dumps(ideal_value) if isinstance(ideal_value, (dict, list)) else ideal_value
+
+    content = {
+        "input": (
+            f"\n"
+            f"Context: {context_str}\n\n"
+            f"Question: ${question}\n\n"
+            f"{instruction['get_tiktok_video_comments']}"
+        ),
+        "ideal": ideal_value_str
+    }
+
+    registry_data += json.dumps(content) + "\n"
+        
+    return registry_data
