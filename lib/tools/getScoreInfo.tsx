216968d2944fa591a9a992b78e9f9a695303b03f
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-11-20 18:49:03 -0800

    I fix trigger function.

--- lib/tools/getScoreInfo.tsx
+++ lib/tools/getScoreInfo.tsx
@@ -5,10 +5,10 @@ import getCampaignScore from "../chat/getCampaignScore";
 const getScoreInfo = (question: string) =>
   tool({
     description: `IMPORTANT: Always call this tool for ANY question related to:
-    1. Highest Scored Fan
-    2. Score Info
-    3. Most Played Fan
-    5. Most Recent Played Fan
+    1. Highest Scored Player
+    2. Players' Score Info
+    3. Most Played Player
+    5. Most Recent Played Player
     Do NOT attempt to answer questions on these topics without consulting this tool first.
 
     Example Questions that MUST trigger this tool:
