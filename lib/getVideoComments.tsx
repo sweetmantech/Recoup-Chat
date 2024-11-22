e3947dd99d50da34b85a7fd1155e2cefcc050eac
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-11-22 13:39:25 -0800

    I fix comments count

--- lib/getVideoComments.tsx
+++ lib/getVideoComments.tsx
@@ -4,15 +4,16 @@ const getVideoComments = async (videoUrls: string) => {
   );
   const data = await response.json();
   const datasetId = data.data;
+  let hasComments = false;
+
   while (1) {
     await new Promise((resolve) => setTimeout(resolve, 6000));
     const response = await fetch(
       `/api/get_tiktok_video_comments/get_dataset_items?datasetId=${datasetId}`,
     );
     const data = await response.json();
-    if (data.data.videos.length > 0) {
-      return data.data;
-    }
+    if (hasComments) return data.data;
+    if (data.data.videos.length > 0) hasComments = true;
   }
 };
 
