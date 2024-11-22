a9403460719559db03e02d8561ce658aff31fe2d
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-11-22 13:51:07 -0800

    I fix comments size

--- lib/getVideoComments.tsx
+++ lib/getVideoComments.tsx
@@ -4,16 +4,20 @@ const getVideoComments = async (videoUrls: string) => {
   );
   const data = await response.json();
   const datasetId = data.data;
-  let counter = 0;
 
   while (1) {
-    await new Promise((resolve) => setTimeout(resolve, 6000));
-    const response = await fetch(
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    let response = await fetch(
       `/api/get_tiktok_video_comments/get_dataset_items?datasetId=${datasetId}`,
     );
-    const data = await response.json();
-    if (counter === 2) return data.data;
-    if (data.data.videos.length > 0) counter++;
+    let data = await response.json();
+    const videos = data.data.videos;
+    response = await fetch(
+      `/api/get_tiktok_video_comments/get_dataset_status?datasetId=${datasetId}`,
+    );
+    data = await response.json();
+    const status = data.data;
+    if (videos?.length && status === "SUCCEED") return data.data;
   }
 };
 
