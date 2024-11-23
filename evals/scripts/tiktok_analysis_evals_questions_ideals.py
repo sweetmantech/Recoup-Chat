7a805539ad89ed7040edbee75d6ef239846f1f3d
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-11-23 12:58:55 -0800

    feat

--- evals/scripts/tiktok_analysis_evals_questions_ideals.py
+++ evals/scripts/tiktok_analysis_evals_questions_ideals.py
@@ -2,7 +2,7 @@ from criteria import count_query_criteria, tiktok_analysis_criteria
 
 evals_questions_ideals = [
     {
-        "eval_name": "analysis_tiktok_profile",
+        "eval_name": "get_tiktok_analysis",
         "question": "Analyze a tiktok account? username is officialluhtyler",
         "ideal": "",
         "criteria": tiktok_analysis_criteria
