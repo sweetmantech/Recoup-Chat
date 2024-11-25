8b24df4c287a87e10cebd0ae2d0c5fa4c1b20780
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-11-24 18:56:53 -0800

    I fix error

--- app/api/tiktok_analysis/save/route.tsx
+++ app/api/tiktok_analysis/save/route.tsx
@@ -5,14 +5,16 @@ export async function POST(req: NextRequest) {
   const body = await req.json();
   try {
     const client = getSupabaseServerAdminClient();
-    const { data } = await client
+    const { data, error } = await client
       .from("tiktok_analysis")
       .insert({
         ...body,
       })
       .select("*")
       .single();
-    return Response.json({ success: true, id: data.id }, { status: 200 });
+    if (error)
+      return Response.json({ id: null, success: false }, { status: 500 });
+    return Response.json({ success: true, id: data?.id }, { status: 200 });
   } catch (error) {
     console.error(error);
     return Response.json({ id: null, success: false }, { status: 500 });
