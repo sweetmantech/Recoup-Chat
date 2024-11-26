a865273fd1b1c8b05c011431330d0e567dd9fc91
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-11-25 23:43:06 -0800

    I fix side menu

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,15 +1,22 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MiniMenu from "./MiniMenu";
 import { motion } from "framer-motion";
 import Menu from "./Menu";
 import useSideMenuAnimation from "@/hooks/useSideMenuAnimation";
+import { useUserProvider } from "@/providers/UserProvder";
 
 const Sidebar = () => {
+  const { email } = useUserProvider();
+
   const [menuExpanded, setMenuExpanded] = useState(false);
   const { animate, initial } = useSideMenuAnimation(menuExpanded);
   const toggleMenuExpanded = () => setMenuExpanded(!menuExpanded);
+
+  useEffect(() => {
+    if (email) setMenuExpanded(true);
+  }, [email]);
 
   return (
     <motion.div
