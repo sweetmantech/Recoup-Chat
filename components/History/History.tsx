433f3bb0ae117e64bfcab0e48b3f4cb456674ff7
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-11-25 23:38:44 -0800

    I fix container

--- components/History/History.tsx
+++ components/History/History.tsx
@@ -6,18 +6,18 @@ const History = () => {
   const { conversations } = useConversations();
 
   return (
-    <div
-      className={`grow h-screen overflow-hidden flex flex-col gap-3 items-center pt-6 md:pt-20 px-4 md:px-0`}
-    >
-      <SearchInput />
-      <div className="max-w-3xl mx-auto w-full space-y-3">
-        {conversations.map((conversation) => (
-          <Conversation
-            key={conversation.metadata.id}
-            name={conversation.metadata.content}
-            id={conversation.metadata.conversationId}
-          />
-        ))}
+    <div className="grow h-screen overflow-hidden bg-background p-4">
+      <div className="size-full bg-white rounded-xl flex flex-col gap-3 items-center pt-6 md:pt-20 px-4 md:px-0">
+        <SearchInput />
+        <div className="max-w-3xl mx-auto w-full space-y-3">
+          {conversations.map((conversation) => (
+            <Conversation
+              key={conversation.metadata.id}
+              name={conversation.metadata.content}
+              id={conversation.metadata.conversationId}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
