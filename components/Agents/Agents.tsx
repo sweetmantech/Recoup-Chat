515b9d71f0520ce9a48dabdddedb59b590ce859b
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-11-25 23:55:23 -0800

    WIP

--- components/Agents/Agents.tsx
+++ components/Agents/Agents.tsx
@@ -1,5 +1,9 @@
 const Agents = () => {
-  return <></>;
+  return (
+    <div className="grow h-screen overflow-hidden bg-background p-4">
+      <div className="size-full bg-white rounded-xl flex flex-col gap-3 items-center pt-6 md:pt-20 px-4 md:px-0"></div>
+    </div>
+  );
 };
 
 export default Agents;
