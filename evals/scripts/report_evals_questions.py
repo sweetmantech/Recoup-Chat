6a151ff35bdf810e357a92adf45e863792d7366d
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-11-28 22:52:20 +0900

    I add new eval

---
+++ evals/scripts/report_evals_questions.py
@@ -0,0 +1,10 @@
+from criteria import segments_report_criteria
+
+evals_questions_ideals = [
+    {
+        "eval_name": "get_segments_report",
+        "question": "Please, create a tiktok fan segment report.",
+        "ideal": "",
+        "criteria": segments_report_criteria
+    }
+]
