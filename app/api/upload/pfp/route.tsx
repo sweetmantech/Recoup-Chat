c975fb7455c4afe48b8737b597ea18bb0dcb81c7
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-12-02 14:19:51 +0900

    check: blob function

--- app/api/upload/pfp/route.tsx
+++ app/api/upload/pfp/route.tsx
@@ -1,12 +1,14 @@
-import uploadPfpToIpfs from "@/lib/uploadPfpToIpfs";
+import getBlob from "@/lib/getBlob";
+// import uploadPfpToIpfs from "@/lib/uploadPfpToIpfs";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
   const url = body.image;
   try {
-    const pfp = await uploadPfpToIpfs(url);
-    return Response.json({ success: true, image: pfp }, { status: 200 });
+    const avatarBlobBuffer = await getBlob(url);
+    // const pfp = await uploadPfpToIpfs(url);
+    return Response.json({ success: true, avatarBlobBuffer }, { status: 200 });
   } catch (error) {
     console.error(error);
     return Response.json({ image: null, success: false }, { status: 500 });
