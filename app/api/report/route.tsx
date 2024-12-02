9509cc97836d2a407c811bc25dd464d7a75c63f6
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-12-03 02:35:15 +0900

    update: report api as stream

--- app/api/report/route.tsx
+++ app/api/report/route.tsx
@@ -4,40 +4,53 @@ import {
   HTML_RESPONSE_FORMAT_INSTRUCTIONS,
 } from "@/lib/consts";
 import { NextRequest } from "next/server";
-import OpenAI from "openai";
 import instructions from "@/evals/scripts/instructions.json";
+import { LanguageModelV1, streamText } from "ai";
+import { openai } from "@ai-sdk/openai";
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
 
   try {
-    const openai = new OpenAI();
-
-    const response = await openai.chat.completions.create({
-      model: AI_MODEL,
-      max_tokens: 1111,
+    const result = await streamText({
+      model: openai(AI_MODEL) as LanguageModelV1,
+      system: `Context: ${JSON.stringify(body)}
+      Question: Please, create a tiktok fan segment report.
+      ${instructions.get_segements_report}
+      ${HTML_RESPONSE_FORMAT_INSTRUCTIONS}
+      NOTE: ${FULL_REPORT_NOTE}`,
+      maxTokens: 1111,
       temperature: 0.7,
-      messages: [
-        {
-          role: "user",
-          content: `Context: ${JSON.stringify(body)}
-          Question: Please, create a tiktok fan segment report.`,
-        },
-        {
-          role: "system",
-          content: `${instructions.get_segements_report}
-          ${HTML_RESPONSE_FORMAT_INSTRUCTIONS}
-          NOTE: ${FULL_REPORT_NOTE}`,
-        },
-      ],
-      store: true,
+      messages: [],
     });
+    // const openai = new OpenAI();
 
-    const content = response.choices[0].message!.content!.toString();
-    return Response.json({
-      message: "success",
-      content,
-    });
+    // const response = await openai.chat.completions.create({
+    //   model: AI_MODEL,
+    //   max_tokens: 1111,
+    //   temperature: 0.7,
+    //   messages: [
+    //     {
+    //       role: "user",
+    //       content: `Context: ${JSON.stringify(body)}
+    //       Question: Please, create a tiktok fan segment report.`,
+    //     },
+    //     {
+    //       role: "system",
+    //       content: `${instructions.get_segements_report}
+    //       ${HTML_RESPONSE_FORMAT_INSTRUCTIONS}
+    //       NOTE: ${FULL_REPORT_NOTE}`,
+    //     },
+    //   ],
+    //   store: true,
+    // });
+
+    // const content = response.choices[0].message!.content!.toString();
+    // return Response.json({
+    //   message: "success",
+    //   content,
+    // });
+    return result.toDataStreamResponse();
   } catch (error) {
     console.error(error);
     const message = error instanceof Error ? error.message : "failed";
