72d917d15393c5438e28143bdce7822416c14989
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-12-04 03:25:23 +0900

    fix: li

--- lib/formatPdf.tsx
+++ lib/formatPdf.tsx
@@ -5,8 +5,12 @@ const formatPdf = (contentHTML: string) => {
     .replaceAll(/<li(.*?)>(.*?)<\/li>/g, (_1, _2, content) => {
       return `<div style="display:flex; gap: 4px;">${dotHTML}${content}</div>`;
     })
-    .replaceAll("<ul", "<div")
-    .replaceAll("</ul>", "</div>");
+    .replaceAll(/<li(.*?)>(.*?)<\//g, (_1, _2, content) => {
+      return `<div style="display:flex; gap: 4px;">${dotHTML}${content}</div>`;
+    })
+    .replaceAll(/<ul(.*?)>(.*?)<\/ul>/g, (_1, _2, content) => {
+      return `<div>${content}</div>`;
+    });
 
   return newHTML;
 };
