a4f61c5103ce6f7049aa3a11b248ac248451d930
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-12-04 02:41:40 +0900

    add: pt

--- lib/formatPdf.tsx
+++ lib/formatPdf.tsx
@@ -1,5 +1,5 @@
 const formatPdf = (contentHTML: string) => {
-  const dotHTML = `<div style="padding-top: 10px; height: 100%; border: 1px solid red;"><div style="width: 5px; height:5px; border-radius:50%; background:black;"></div></div>`;
+  const dotHTML = `<div style="padding-top: 20px; height: 100%; border: 1px solid red;"><div style="width: 5px; height:5px; border-radius:50%; background:black;"></div></div>`;
 
   const newHTML = contentHTML
     .replaceAll(/<li>(.*?)<\/li>/g, (_, content) => {
