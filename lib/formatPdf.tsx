b145beeb1273b86d49d02a6d6a4dc3ac3f03b7d3
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-12-04 02:05:17 +0900

    fix: list px

--- lib/formatPdf.tsx
+++ lib/formatPdf.tsx
@@ -1,14 +1,14 @@
 const formatPdf = (contentHTML: string) => {
-  const dotHTML = `<div style="width: 5px; height:5px; border-radius:50%; background:black; margin-top: 10px;"></div>`;
+  const dotHTML = `<div style="width: 5px; height:5px; border-radius:50%; background:black;"></div>`;
 
   const newHTML = contentHTML
     .replaceAll(/<li>(.*?)<\/li>/g, (_, content) => {
-      return `<div style="display:flex; gap: 4px;">${dotHTML}${content}</div>`;
+      return `<div style="display:flex; gap: 4px;">${dotHTML}<div style="padding-bottom: 15px;">${content}</div></div>`;
     })
     .replaceAll(
       /<li style="list-style: inside;">(.*?)<\/li>/g,
       (_, content) => {
-        return `<div style="display:flex; gap: 4px;">${dotHTML}${content}</div>`;
+        return `<div style="display:flex; gap: 4px;">${dotHTML}<div style="padding-bottom: 15px;">${content}</div></div>`;
       },
     )
     .replaceAll("<ul", "<div")
