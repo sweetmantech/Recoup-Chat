8c3978125f5931b087e0dde3365e4bca35702eca
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-12-04 02:53:08 +0900

    fix

--- lib/formatPdf.tsx
+++ lib/formatPdf.tsx
@@ -2,7 +2,7 @@ const formatPdf = (contentHTML: string) => {
   const dotHTML = `<div style="padding-top: 20px; height: 100%; border: 1px solid red;"><div style="width: 5px; height:5px; border-radius:50%; background:black;"></div></div>`;
 
   const newHTML = contentHTML
-    .replaceAll(/<li(.*?)>(.*?)<\/li>/g, (_, content) => {
+    .replaceAll(/<li(.*?)>(.*?)<\/li>/g, (_1, _2, content) => {
       return `<div style="display:flex; gap: 4px; border: 1px solid red;">${dotHTML}${content}</div>`;
     })
     .replaceAll("<ul", "<div")
