2b16fea79a3c81acb80acfc86ff08039c2eab903
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-12-05 22:19:56 +0300

    fix: chat logo icon

--- components/Chat/Thinking.tsx
+++ components/Chat/Thinking.tsx
@@ -1,13 +1,14 @@
 import getThinkingStatus from "@/lib/getThinkingStatus";
 import { useChatProvider } from "@/providers/ChatProvider";
-import { LoaderCircle, TvMinimalPlay } from "lucide-react";
+import { LoaderCircle } from "lucide-react";
+import Icon from "../Icon";
 
 const Thinking = () => {
   const { toolCall } = useChatProvider();
   return (
     <div className="p-3 flex gap-2 w-full max-w-3xl mx-auto items-center pb-2">
-      <div className="size-fit">
-        <TvMinimalPlay className="h-6 w-6" />
+      <div className="p-2 border border-grey rounded-full">
+        <Icon name="logo-xs" />
       </div>
       <p className="text-sm">{getThinkingStatus(toolCall)}</p>
       <LoaderCircle className="h-4 w-4 animate-spin" />
