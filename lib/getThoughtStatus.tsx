acf1957c9f3dbd088c628282d425d34e42eb05d3
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-12-05 20:05:52 +0300

    fix: empty text.

--- lib/getThoughtStatus.tsx
+++ lib/getThoughtStatus.tsx
@@ -2,7 +2,7 @@ const getThoughtStatus = (progress: number) => {
   if (progress < 40) return `Looking at overlays and captions.`;
   if (progress < 60) return `Calculating engagement rates for recent posts.`;
   if (progress < 80) return `Analyzing comments on videos.`;
-  if (progress < 100) return `Finalizing artist analysis.`;
+  return `Finalizing artist analysis.`;
 };
 
 export default getThoughtStatus;
