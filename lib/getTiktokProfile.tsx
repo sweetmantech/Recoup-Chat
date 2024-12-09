6f82f4a78ef2b7cbbcb4c8b8cda075cc0700fbae
Va~~~dym <tech.eng.me@gmail.com>
2024-12-09 14:35:45 -0800

    fix: feedback.

--- lib/getTiktokProfile.tsx
+++ lib/getTiktokProfile.tsx
@@ -3,32 +3,28 @@ import { AGENT_API } from "./consts";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const getTikTokProfile = async (handle: string, setThought?: any) => {
-  try {
-    const response = await fetch(
-      `${AGENT_API}/api/get_tiktok_account_trends?handle=${handle}`,
-    );
-    if (!response.ok) throw Error("failed!");
-    const data = await response.json();
-    const datasetId = data.data;
+  const response = await fetch(
+    `${AGENT_API}/api/get_tiktok_account_trends?handle=${handle}`,
+  );
+  if (!response.ok) throw Error("failed!");
+  const data = await response.json();
+  const datasetId = data.data;
 
-    while (1) {
-      await new Promise((resolve) => setTimeout(resolve, 3000));
-      if (setThought) setThought(STEP_OF_ANALYSIS.PROFILE);
-      const datasetItemsRes = await fetch(
-        `${AGENT_API}/api/get_tiktok_account_trends/get_dataset_items?datasetId=${datasetId}`,
-      );
-      if (!datasetItemsRes.ok) throw Error("failed!");
-      const datasetItems = await datasetItemsRes.json();
-      const profileInfo = datasetItems.data;
-      const statusRes = await fetch(
-        `${AGENT_API}/api/get_tiktok_account_trends/get_dataset_status?datasetId=${datasetId}`,
-      );
-      const statusInfo = await statusRes.json();
-      const status = statusInfo.data;
-      if (profileInfo && status === "SUCCEEDED") return profileInfo;
-    }
-  } catch (error) {
-    console.error("ZIAD", error);
+  while (1) {
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    if (setThought) setThought(STEP_OF_ANALYSIS.PROFILE);
+    const datasetItemsRes = await fetch(
+      `${AGENT_API}/api/get_tiktok_account_trends/get_dataset_items?datasetId=${datasetId}`,
+    );
+    if (!datasetItemsRes.ok) throw Error("failed!");
+    const datasetItems = await datasetItemsRes.json();
+    const profileInfo = datasetItems.data;
+    const statusRes = await fetch(
+      `${AGENT_API}/api/get_tiktok_account_trends/get_dataset_status?datasetId=${datasetId}`,
+    );
+    const statusInfo = await statusRes.json();
+    const status = statusInfo.data;
+    if (profileInfo && status === "SUCCEEDED") return profileInfo;
   }
 };
 
