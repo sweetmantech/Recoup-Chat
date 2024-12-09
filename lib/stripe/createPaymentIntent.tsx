68df7d032a28d56c99335d87830ef242c999385f
Va~~~dym <tech.eng.me@gmail.com>
2024-12-08 21:19:31 -0800

    fix: env

--- lib/stripe/createPaymentIntent.tsx
+++ lib/stripe/createPaymentIntent.tsx
@@ -2,25 +2,15 @@ import handleError from "../handleError";
 
 export const createPaymentIntent = async () => {
   try {
-    const paymentIntentData = {
-      amount: 99,
-      currency: "usd",
-      "automatic_payment_methods[enabled]": "true",
-      "metadata[created_at]": Date.now(),
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } as any;
-
-    const response = await fetch(`https://api.stripe.com/v1/payment_intents`, {
-      method: "POST",
-      body: new URLSearchParams(paymentIntentData).toString(),
+    const response = await fetch(`/api/stripe/create_payment_intent`, {
+      method: "GET",
       headers: {
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRIPE_SK}`,
-        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Type": "application/json",
       },
     });
 
     const data = await response.json();
-    return data;
+    return data.data;
   } catch (error) {
     handleError(error);
     return { error };
