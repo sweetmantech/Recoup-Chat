5425b2420d527c0d5d7808c5e39eb2e8dd9045fe
Va~~~dym <tech.eng.me@gmail.com>
2024-12-11 13:52:13 -0800

    WIP

--- app/api/stripe/create_price/route.ts
+++ app/api/stripe/create_price/route.ts
@@ -6,6 +6,7 @@ export async function GET(req: NextRequest) {
   const priceAmount = req.nextUrl.searchParams.get("price");
 
   try {
+    const isSubscription = priceAmount === "2000";
     const price = await stripeClient.prices.create({
       currency: "usd",
       unit_amount: priceAmount,
@@ -13,6 +14,17 @@ export async function GET(req: NextRequest) {
         name: productName,
       },
     });
+
+    if (isSubscription) {
+      price.type = "recurring";
+      price.recurring = {
+        aggregate_usage: null,
+        interval: "month",
+        interval_count: 1,
+        trial_period_days: null,
+        usage_type: "licensed",
+      };
+    }
 
     return Response.json({ data: price }, { status: 200 });
   } catch (error) {
