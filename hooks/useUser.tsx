d643a5ae845a3e31511495a737ed1b268066dc3b
Va~~~dym <tech.eng.me@gmail.com>
2024-12-11 14:39:26 -0800

    chore: close modal after updating account data

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -51,6 +51,7 @@ const useUser = () => {
     const data = await response.json();
     setUserData(data.data);
     setUpdating(false);
+    setIsModalOpen(false);
   };
 
   const isPrepared = () => {
