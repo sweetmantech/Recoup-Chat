80da91d2bbb9ad5cef3835c6e0f1c8f34fc3c72b
Va~~~dym <tech.eng.me@gmail.com>
2024-12-15 20:36:00 -0800

    fix: padding

--- components/Chat/ReportSummaryNote.tsx
+++ components/Chat/ReportSummaryNote.tsx
@@ -32,7 +32,7 @@ const ReportSummaryNote = () => {
         <div className="bg-white w-full min-h-screen fixed top-[99999999px] left-0 flex justify-center z-[99999999]">
           <div
             id="segment-report"
-            className="flex flex-col text-black max-w-[9.5in] w-full bg-white p-[0.3in] text-[11pt] leading-normal relative box-border min-h-[11in]"
+            className="flex flex-col text-black max-w-[9.5in] w-full bg-white pt-[0.3in] pb-[0.5in] px-[0.3in] text-[11pt] leading-normal relative box-border min-h-[11in]"
           >
             <div className="grow w-full">
               <div className="w-full aspect-[757/146] rounded-lg flex items-center justify-center overflow-hidden relative mb-6">
