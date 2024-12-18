74d832f9c368530e6b565ff5d5df962f29ffd7d8
Va~~~dym <tech.eng.me@gmail.com>
2024-12-18 10:33:18 -0800

    fix: route

--- hooks/useTwitterAnalysisChain.tsx
+++ hooks/useTwitterAnalysisChain.tsx
@@ -2,17 +2,28 @@ import { useUserProvider } from "@/providers/UserProvder";
 import { STEP_OF_ANALYSIS } from "@/types/TikTok";
 import { useFunnelAnalysisProvider } from "@/providers/FunnelAnalysisProvider";
 import getTwitterProfile from "@/lib/twitter/getTwitterProfile";
+import { useRouter } from "next/navigation";
+import { v4 as uuidV4 } from "uuid";
 
 const useTwitterAnalysisChain = () => {
-  const { setIsLoading, setThought, username, isLoading, setResult } =
-    useFunnelAnalysisProvider();
+  const {
+    setIsLoading,
+    setThought,
+    username,
+    isLoading,
+    setResult,
+    funnelType,
+  } = useFunnelAnalysisProvider();
   const { isPrepared } = useUserProvider();
+  const { push } = useRouter();
 
   const handleAnalyze = async () => {
     try {
       if (!isPrepared()) return;
       setIsLoading(true);
       if (!username || isLoading) return;
+      const newId = uuidV4();
+      push(`/funnels/${funnelType}/${newId}`);
       await new Promise((resolve) => setTimeout(resolve, 1900));
       setThought(STEP_OF_ANALYSIS.PROFILE);
       const profile = await getTwitterProfile(username.replaceAll("@", ""));
