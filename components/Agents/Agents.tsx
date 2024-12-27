cddda5674dd64a371d5b70713da9a4a4dc9dba86
Va~~~dym <tech.eng.me@gmail.com>
2024-12-27 15:59:14 -0800

    fix: feedback

--- components/Agents/Agents.tsx
+++ components/Agents/Agents.tsx
@@ -1,6 +1,9 @@
+import { useRouter } from "next/navigation";
 import FunnelCard from "./FunnelCard";
 
 const Agents = () => {
+  const { push } = useRouter();
+
   return (
     <div className="grow h-screen overflow-hidden md:bg-background md:p-4">
       <div className="size-full bg-white overflow-y-auto pb-20 md:pb-0 rounded-xl flex flex-col items-center md:items-start gap-3 pt-6 md:pt-10 px-4 md:px-10">
@@ -13,6 +16,7 @@ const Agents = () => {
           <button
             type="button"
             className="w-full w-full h-[162px] overflow-hidden rounded-xl"
+            onClick={() => push(`/funnels/wrapped`)}
           >
             <div className="relative bg-[url('/wrapped.png')] bg-cover bg-center size-full flex flex-col items-start justify-end pb-4 pl-4">
               <p className="text-white text-2xl md:text-[40px] pb-1 md:pb-2 text-left font-plus_jakarta_sans_bold">
