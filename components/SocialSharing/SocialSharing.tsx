7789b4c97a8d01ef24115436dfbe3028a1eff03f
Va~~~dym <tech.eng.me@gmail.com>
2025-01-09 11:02:34 -0800

    add: hook

--- components/SocialSharing/SocialSharing.tsx
+++ components/SocialSharing/SocialSharing.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import HeatMap from "@uiw/react-heat-map";
 import useActivities from "@/hooks/useActivities";
 import { Twitter, Download } from "lucide-react";
@@ -11,10 +11,12 @@ const SocialSharing = () => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const heatmap = useRef() as any;
   const [loading, setLoading] = useState(false);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [blob, setBlob] = useState<any>(null);
 
   const download = async () => {
+    if (!blob) return;
     setLoading(true);
-    const blob = await domtoimage.toBlob(heatmap.current);
     const url = URL.createObjectURL(blob);
     const a = document.createElement("a");
     a.href = url;
@@ -26,8 +28,8 @@ const SocialSharing = () => {
   };
 
   const tweets = async () => {
+    if (!blob) return;
     setLoading(true);
-    const blob = await domtoimage.toBlob(heatmap.current);
     const fileName = "heatmap.png";
     const fileType = "image/png";
     const mapFile = new File([blob], fileName, { type: fileType });
@@ -36,6 +38,16 @@ const SocialSharing = () => {
     window.open(tweetLink, "_blank");
     setLoading(false);
   };
+
+  useEffect(() => {
+    const init = async () => {
+      const domBlob = await domtoimage.toBlob(heatmap.current);
+      setBlob(domBlob);
+    };
+    if (!heatmap.current) return;
+
+    init();
+  }, [heatmap]);
 
   return (
     <div className="w-full mt-4">
