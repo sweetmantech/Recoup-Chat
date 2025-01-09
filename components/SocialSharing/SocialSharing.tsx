86e0d470f111f1586909c1c2959be5a9d65bce69
Va~~~dym <tech.eng.me@gmail.com>
2025-01-09 13:11:18 -0800

    add tooltip

--- components/SocialSharing/SocialSharing.tsx
+++ components/SocialSharing/SocialSharing.tsx
@@ -1,56 +1,15 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import HeatMap from "@uiw/react-heat-map";
 import useActivities from "@/hooks/useActivities";
 import { Twitter, Download } from "lucide-react";
-import domtoimage from "dom-to-image";
-import { uploadFile } from "@/lib/ipfs/uploadToIpfs";
-import getIpfsLink from "@/lib/ipfs/getIpfsLink";
 import { ONE_DAY_MILLISECONDS } from "@/lib/consts";
-import Tooltip from '@uiw/react-tooltip';
+import Tooltip from "@uiw/react-tooltip";
+import useShareHeatMap from "@/hooks/useShareHeatMap";
 
 const SocialSharing = () => {
   const today = new Date();
   const { activities } = useActivities();
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const heatmap = useRef() as any;
-  const [loading, setLoading] = useState(false);
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const [blob, setBlob] = useState<any>(null);
-
-  const download = async () => {
-    if (!blob) return;
-    setLoading(true);
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = "heatmap.png";
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-    setLoading(false);
-  };
-
-  const tweets = async () => {
-    if (!blob) return;
-    setLoading(true);
-    const fileName = "heatmap.png";
-    const fileType = "image/png";
-    const mapFile = new File([blob], fileName, { type: fileType });
-    const { cid } = await uploadFile(mapFile);
-    const tweetLink = `https://x.com/intent/tweet?text=${encodeURIComponent(getIpfsLink(`ipfs://${cid}`))}`;
-    window.open(tweetLink, "_blank");
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    const init = async () => {
-      const domBlob = await domtoimage.toBlob(heatmap.current);
-      setBlob(domBlob);
-    };
-    if (!heatmap.current) return;
-    init();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [heatmap.current]);
+  const { heatmap, download, tweets, loading } = useShareHeatMap();
 
   return (
     <div className="w-full mt-4">
