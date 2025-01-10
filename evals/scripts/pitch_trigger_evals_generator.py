695b2557e1b959020a80382af945934bbd10cf47
Va~~~dym <tech.eng.me@gmail.com>
2025-01-10 00:27:15 -0800

    I update ideal value

--- evals/scripts/pitch_trigger_evals_generator.py
+++ evals/scripts/pitch_trigger_evals_generator.py
@@ -9,10 +9,16 @@ instruction = get_instruction()
 content = {
     "input": (
         f"\n"
-        f"Question: how can I best pitch Logic Pro?\n\n"
-        f"{instruction['pitch_report_trigger']}"
+        f"Question:\n"
+        f"1.How can I best pitch Logic Pro?\n"
+        f"2.What is the listening habits?\n"
+        f"3.Analyze a tiktok account.\n\n"
+        f"4.Write Celebrity Relationship Commentators Pitch."
+
+        f"Instruction: {instruction['pitch_report_trigger']}"
+        f"To evaluate the tool's trigger mechanism, Respond only to questions fitting the specified format: '[Full question sentence]: [Pitch Name]'.\n"
     ),
-    "ideal": ""
+    "ideal": "Answer should include these words - Logic Pro, Celebrity Relationship Commentators"
 }
 registry_data = json.dumps(content)
 create_data("pitch_question_tool_trigger", registry_data)
