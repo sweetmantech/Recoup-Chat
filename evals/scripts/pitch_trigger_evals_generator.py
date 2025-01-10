b0248d8fde1893e89f080b7666edcd1dc1a7404f
Va~~~dym <tech.eng.me@gmail.com>
2025-01-10 00:29:59 -0800

    I update trigger eval title.

--- evals/scripts/pitch_trigger_evals_generator.py
+++ evals/scripts/pitch_trigger_evals_generator.py
@@ -3,7 +3,7 @@ import string
 from get_instruction import get_instruction
 from create_yaml_data import create_yaml, create_data
 
-create_yaml("pitch_question_tool_trigger", "The answer should be the pitch name.", "how can I best pitch Logic Pro?")
+create_yaml("pitch_question_tool_trigger", "Answer should include ideal values.", "Trigger evaluation of questions related to pitch reports.")
 
 instruction = get_instruction()
 content = {
@@ -18,7 +18,7 @@ content = {
         f"Instruction: {instruction['pitch_report_trigger']}"
         f"To evaluate the tool's trigger mechanism, Respond only to questions fitting the specified format: '[Full question sentence]: [Pitch Name]'.\n"
     ),
-    "ideal": "Answer should include these words - Logic Pro, Celebrity Relationship Commentators"
+    "ideal": "Logic Pro, Celebrity Relationship Commentators"
 }
 registry_data = json.dumps(content)
 create_data("pitch_question_tool_trigger", registry_data)
