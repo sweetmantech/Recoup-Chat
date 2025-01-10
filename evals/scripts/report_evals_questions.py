59eb5e8bb7b476058a9a6f182717e7af8792c6d6
Va~~~dym <tech.eng.me@gmail.com>
2025-01-10 03:52:00 -0800

    I update instruction

--- evals/scripts/report_evals_questions.py
+++ evals/scripts/report_evals_questions.py
@@ -3,14 +3,14 @@ from criteria import segments_report_criteria, segments_report_summary_criteria
 evals_questions_ideals = [
     {
         "eval_name": "get_segments_report",
-        "question": "Please, create a tiktok fan segment report.",
+        "question": "Please, create a fan segment report.",
         "ideal": "",
         "criteria": segments_report_criteria,
         "instruction": "get_segements_report"
     },
     {
         "eval_name": "get_segments_report_summary",
-        "question": "Please, create a tiktok fan segment report summary.",
+        "question": "Please, create a fan segment report summary.",
         "ideal": "",
         "criteria": segments_report_summary_criteria,
         "instruction": "get_segments_report_summary"
