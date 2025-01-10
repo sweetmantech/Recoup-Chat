597bd3d154a699fd80bfe5d9dbb00f1b5bd06fa4
Va~~~dym <tech.eng.me@gmail.com>
2025-01-10 00:09:36 -0800

    fix: name 'json' is not defined

--- evals/scripts/pitch_trigger_evals_generator.py
+++ evals/scripts/pitch_trigger_evals_generator.py
@@ -1,5 +1,7 @@
-from create_yaml_data import create_yaml, create_data
+import json
+import string
 from get_instruction import get_instruction
+from create_yaml_data import create_yaml, create_data
 
 create_yaml("pitch_question_tool_trigger", "The answer should be the pitch name.", "how can I best pitch Logic Pro?")
 
