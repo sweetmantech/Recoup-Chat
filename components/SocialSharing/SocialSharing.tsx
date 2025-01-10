d13635ad2faab426a0e4bb222f04ee09d325bae6
Va~~~dym <tech.eng.me@gmail.com>
2025-01-09 21:03:16 -0800

    feat: add tooltip of onHover

--- components/SocialSharing/SocialSharing.tsx
+++ components/SocialSharing/SocialSharing.tsx
@@ -1,34 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import HeatMap from "@uiw/react-heat-map";
 import useActivities from "@/hooks/useActivities";
 import { Twitter, Download } from "lucide-react";
 import { ONE_DAY_MILLISECONDS } from "@/lib/consts";
-import Tooltip from "@uiw/react-tooltip";
 import useShareHeatMap from "@/hooks/useShareHeatMap";
 
 const SocialSharing = () => {
   const today = new Date();
   const { activities } = useActivities();
   const { heatmap, download, tweets, loading } = useShareHeatMap();
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [tooltipActive, setTooltipActive] = useState(false);
+  const [tooltipContent, setTooltipContent] = useState("");
 
   return (
     <div className="w-full mt-4">
       {activities?.length > 0 && (
         <>
-          <div ref={heatmap}>
+          <div ref={heatmap} className="relative">
             <HeatMap
               value={activities}
+              width={600}
               weekLabels={["", "Mon", "", "Wed", "", "Fri", ""]}
               startDate={new Date(today.getTime() - 330 * ONE_DAY_MILLISECONDS)}
               className="w-full"
               rectRender={(props, data) => {
+                if (!data.count) return <rect {...props} />;
                 return (
-                  <Tooltip placement="top" content={`${data.count || 0}`}>
-                    <rect {...props} />
-                  </Tooltip>
+                  <rect
+                    {...props}
+                    onMouseOver={() => {
+                      setX(Number(props?.x));
+                      setY(Number(props?.y));
+                      setTooltipContent(data.count.toString());
+                      setTooltipActive(true);
+                    }}
+                    onMouseLeave={() => {
+                      setTooltipActive(false);
+                      setX(-1000000);
+                      setY(-1000000);
+                      setTooltipContent("");
+                    }}
+                  />
                 );
               }}
             />
+            {tooltipActive && (
+              <div
+                className="absolute px-1 bg-black text-white text-[9px] rounded-md"
+                style={{
+                  left: x + 23,
+                  top: y - 2,
+                }}
+              >
+                {tooltipContent}
+              </div>
+            )}
           </div>
           <div className="flex gap-2 items-2 w-full justify-end">
             <button
