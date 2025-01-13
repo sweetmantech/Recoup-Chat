f29e42559ba0322ae1d9cb2ac65efa83819accd1
Va~~~dym <tech.eng.me@gmail.com>
2025-01-12 18:37:11 -0800

    I fix endpoint

--- evals/scripts/consts.py
+++ evals/scripts/consts.py
@@ -2,4 +2,4 @@ mock_data = [
     ('a4c897f5-02cb-43d3-b971-948c85537785', 'sidney@syncstream.ai'),
     ('a4c897f5-02cb-43d3-b971-948c85537785', 'sweetmantech@gmail.com'),
 ]
-api_endpoint = "https://recoup-chat-git-tech322-instrumental-0f1612-recoupable-ad724970.vercel.app"
+api_endpoint = "https://chat.recoupable.com"
