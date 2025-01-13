30eae1a9991946ab5f6fdaeff8b5eca9b39e375d
Va~~~dym <tech.eng.me@gmail.com>
2025-01-12 18:02:15 -0800

    I update instruction.

--- evals/scripts/analysis_prompts_evals_generator.py
+++ evals/scripts/analysis_prompts_evals_generator.py
@@ -4,7 +4,7 @@ from get_instruction import get_instruction
 from create_yaml_data import create_yaml, create_data
 from get_pitch_report_context import get_pitch_report_context
 
-create_yaml("instrumental_suggestion_tool_trigger", "Answer should be the prompts.", "Trigger evaluation of questions related to funnel analysis.")
+create_yaml("analysis_suggestion_tool_trigger", "Answer should be the prompts.", "Funnel Analysis Suggestions.")
 
 instruction = get_instruction()
 context = get_pitch_report_context()
@@ -13,10 +13,10 @@ context_str = json.dumps(context)
 content = {
     "input": (
         f"\n"
-        f"[Instruction]: {instruction['get_instrumental_suggestion']}\n\n\n"
+        f"[Instruction]: {instruction['get_analysis_suggestion']}\n\n\n"
         f"[Context]: {context_str}"
     ),
-    "ideal": "Logic Pro, Celebrity Relationship Commentators"
+    "ideal": ""
 }
 registry_data = json.dumps(content)
-create_data("instrumental_suggestion_tool_trigger", registry_data)
+create_data("analysis_suggestion_tool_trigger", registry_data)
