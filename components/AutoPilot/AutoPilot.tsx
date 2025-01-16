c65de40ae1d63e1f7fc44feb0f1d1c9859621115
Va~~~dym <tech.eng.me@gmail.com>
2025-01-16 13:43:39 -0800

    I fix logged out state autopilot

--- components/AutoPilot/AutoPilot.tsx
+++ components/AutoPilot/AutoPilot.tsx
@@ -7,9 +7,11 @@ import Artists from "./Artists";
 import Events from "./Events";
 import Approvals from "./Approvals";
 import ArtistsSidebar from "./ArtistsSidebar";
+import { useUserProvider } from "@/providers/UserProvder";
 
 const AutoPilot = () => {
   const { selectedArtist } = useArtistProvider();
+  const { email } = useUserProvider();
 
   return (
     <div className="grow flex h-[calc(100vh-56px)] md:h-screen overflow-hidden md:bg-grey-light-3">
@@ -19,12 +21,12 @@ const AutoPilot = () => {
             <div className="flex items-center gap-2 mb-2 md:mb-4 pb-2 md:pb-4">
               <Terminal className="h-5 w-5" />
               <h1 className="text-md md:text-lg font-plus_jakarta_sans_bold">
-                {selectedArtist
+                {selectedArtist && email
                   ? `${selectedArtist?.name}'s Agent`
                   : "Peek Into Recoup's Brain"}
               </h1>
             </div>
-            {selectedArtist ? (
+            {selectedArtist && email ? (
               <div className="flex flex-col md:block md:grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-4 h-full grow">
                 <div className="space-y-1 md:space-y-4">
                   <CurrentState />
