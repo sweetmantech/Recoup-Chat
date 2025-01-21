c3a576823dc62cc0c59eae1aa89a7da59186f389
Va~~~dym <tech.eng.me@gmail.com>
2025-01-20 23:10:13 -0800

    I fix result copy.

--- components/Funnels/CompletedAnalysis.tsx
+++ components/Funnels/CompletedAnalysis.tsx
@@ -11,7 +11,7 @@ const CompletedAnalysis = () => {
       <p className="text-lg md:text-xl font-bold pb-4">
         <span className="capitalize">{funnelName}</span> Analysis complete✅
       </p>
-      <p>{`${result?.nickname || selectedArtist?.name} has ${result?.followers} followers. Please select a fan segmentation below to generate a report for brand partnership deals.`}</p>
+      <p>{`${result?.nickname || selectedArtist?.name} has ${result?.followers} followers. We've analyzed your most recent 100 engagements in this quick scan. Select a fan segmentation below to generate a detailed report for brand partnership opportunities.`}</p>
       <p className="text-lg md:text-xl text-xl font-bold py-4"> Fan Segments</p>
       <p>{`We categorized ${result?.name}'s fans into ${Object.keys(segments).length} different segments - click any to explore. The agent is running in the background and will notify you of new insights!`}</p>
       {segments?.length > 0 && <Segments />}
