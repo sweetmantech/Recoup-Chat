5b9f7ec25fb1b8ea18067d351423bd273fc1b754
Va~~~dym <tech.eng.me@gmail.com>
2025-01-23 18:09:05 -0800

    I fix artist side bar

--- components/Header/Artist.tsx
+++ components/Header/Artist.tsx
@@ -25,9 +25,8 @@ const Artist = ({
       className={`${
         isMini
           ? `${isSelectedArtist && "w-fit rounded-full shadow-[1px_1px_1px_1px_#E6E6E6]"}`
-          : `flex gap-1 justify-between items-center px-2 py-1 text-sm rounded-md text-grey-dark hover:bg-grey-light-1 
-        ${isSelectedArtist && "!bg-grey-light-1"}`
-      }`}
+          : `flex gap-1 justify-between items-center px-2 py-1 text-sm rounded-md text-grey-dark hover:bg-grey-light-1 ${isSelectedArtist && "!bg-grey-light-1"}`
+      } h-10`}
     >
       <div className="w-8 aspect-1/1 rounded-full overflow-hidden flex items-center justify-center border">
         <ImageWithFallback src={artist?.image || ""} />
