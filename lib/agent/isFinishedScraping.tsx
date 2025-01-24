b01115402ab495442a1619026b4c50c5a588f06f
Va~~~dym <tech.eng.me@gmail.com>
2025-01-24 02:54:58 -0800

    I fix thoughts

--- lib/agent/isFinishedScraping.tsx
+++ lib/agent/isFinishedScraping.tsx
@@ -2,12 +2,17 @@ import { STEP_OF_ANALYSIS } from "@/types/Funnel";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const isFinishedScraping = (thoughts: any) => {
-  return Object.values(thoughts).every(
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (value: any) =>
-      value.status === STEP_OF_ANALYSIS.FINISHED ||
-      value.status === STEP_OF_ANALYSIS.ERROR,
-  );
+  return Object.entries(thoughts)
+    .filter(
+      // eslint-disable-next-line
+      ([id, value]: any) => id !== "wrapped",
+    )
+    .every(
+      // eslint-disable-next-line
+      ([id, value]: any) =>
+        value.status === STEP_OF_ANALYSIS.FINISHED ||
+        value.status === STEP_OF_ANALYSIS.ERROR,
+    );
 };
 
 export default isFinishedScraping;
