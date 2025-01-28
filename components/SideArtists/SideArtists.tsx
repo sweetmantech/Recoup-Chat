1d624da4a8874d4429689c5fe36fa225276248b0
Va~~~dym <tech.eng.me@gmail.com>
2025-01-28 05:32:58 -0800

    I fix hide artist bar

---
+++ components/SideArtists/SideArtists.tsx
@@ -0,0 +1,55 @@
+import SideModal from "../SideModal";
+import { useUserProvider } from "@/providers/UserProvder";
+import Artist from "../Header/Artist";
+import { ArtistRecord } from "@/types/Artist";
+import { useArtistProvider } from "@/providers/ArtistProvider";
+import { Plus } from "lucide-react";
+
+const SideArtists = ({
+  isVisible,
+  toggleModal,
+}: {
+  isVisible: boolean;
+  toggleModal: () => void;
+}) => {
+  const { email, isPrepared } = useUserProvider();
+  const { toggleCreation, toggleSettingModal, sorted } = useArtistProvider();
+
+  const handleCreate = () => {
+    if (!isPrepared()) return;
+
+    toggleCreation();
+    toggleSettingModal();
+  };
+  return (
+    <SideModal
+      isVisible={isVisible}
+      toggleModal={toggleModal}
+      containerClasses="justify-end"
+      direction="right"
+      className="!w-fit"
+    >
+      <div className="no-scrollbar grow flex flex-col gap-1 overflow-y-auto overflow-x-hidden">
+        {email &&
+          sorted.map((artist: ArtistRecord | null) => (
+            <Artist
+              artist={artist}
+              toggleDropDown={() => {}}
+              key={artist?.id}
+            />
+          ))}
+      </div>
+      <button
+        className="flex px-2 py-1 gap-2 text-sm items-center text-grey-dark-1"
+        onClick={handleCreate}
+      >
+        <div className="w-8 flex justify-center">
+          <Plus className="size-5 text-grey-dark-1" />
+        </div>
+        New Artist
+      </button>
+    </SideModal>
+  );
+};
+
+export default SideArtists;
--- hooks/useModalAnimation.tsx
+++ hooks/useModalAnimation.tsx
@@ -1,6 +1,8 @@
-const useModalAnimation = (isOpenModal: boolean) => {
-  const desktopAnimate = { x: isOpenModal ? "0%" : "-100%" };
-  const desktopInitial = { x: 0 };
+const useModalAnimation = (isOpenModal: boolean, direction = "left") => {
+  const desktopAnimate = {
+    x: isOpenModal ? 0 : direction === "left" ? "-100%" : "100%",
+  };
+  const desktopInitial = { x: direction === "left" ? 0 : "100%" };
 
   return {
     animate: desktopAnimate,
