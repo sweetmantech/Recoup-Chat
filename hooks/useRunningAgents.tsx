5c72f56c16ea5212c40238548aac9a0b2b850068
Va~~~dym <tech.eng.me@gmail.com>
2025-02-03 05:05:52 -0800

    fix: current state

--- hooks/useRunningAgents.tsx
+++ hooks/useRunningAgents.tsx
@@ -1,41 +1,40 @@
+import getThoughtStatus from "@/lib/agent/getAgentStatus";
+import getRunningAgent from "@/lib/getRunningAgent";
+import getSocialPlatformByLink from "@/lib/getSocialPlatformByLink";
 import { useArtistProvider } from "@/providers/ArtistProvider";
 import { STEP_OF_AGENT } from "@/types/Funnel";
 import { useCallback, useEffect, useState } from "react";
 
 const useRunningAgents = () => {
-  const [curLiveAgent, setCurLiveAgent] = useState<string | null>(null);
+  const [curLiveAgent, setCurLiveAgent] = useState<any>(null);
   const { selectedArtist } = useArtistProvider();
 
-  const statusMessages: any = (handle: string) => ({
-    [STEP_OF_AGENT.PROFILE]: `Looking at ${handle}'s profile.`,
-    [STEP_OF_AGENT.TRACKS]: `Looking at ${handle}'s tracks.`,
-    [STEP_OF_AGENT.ALBUMS]: `Looking at ${handle}'s albums.`,
-    [STEP_OF_AGENT.POSTURLS]: `Reviewing ${handle}'s top-performing videos.`,
-    [STEP_OF_AGENT.POST_COMMENTS]: `Looking at overlays and captions.`,
-    [STEP_OF_AGENT.SEGMENTS]: `Grouping all of the @${handle}'s Fans into the segments.`,
+  const statusMessages: any = {
+    [STEP_OF_AGENT.PROFILE]: `Looking at ${selectedArtist?.name}'s profile.`,
+    [STEP_OF_AGENT.TRACKS]: `Looking at ${selectedArtist?.name}'s tracks.`,
+    [STEP_OF_AGENT.POST_COMMENTS]: getThoughtStatus(curLiveAgent?.progress),
+    [STEP_OF_AGENT.ALBUMS]: `Looking at ${selectedArtist?.name}'s albums.`,
+    [STEP_OF_AGENT.POSTURLS]: `Reviewing ${selectedArtist?.name}'s top-performing videos.`,
     [STEP_OF_AGENT.SAVING_ANALYSIS]: `Saving video comments scrapped data.`,
     [STEP_OF_AGENT.CREATED_ARTIST]: `Setting up artist mode.`,
-  });
+    [STEP_OF_AGENT.FINISHED]: "",
+  };
 
   const getLiveAgent = useCallback(async () => {
     if (!selectedArtist?.account_id) return;
-    const response = await fetch(
-      `/api/get_running_agents?artistId=${selectedArtist?.account_id || ""}`,
-    );
-
-    const data = await response.json();
-    if (data?.data?.length === 0) {
-      setCurLiveAgent(null);
+    const response = await getRunningAgent(selectedArtist?.account_id);
+    if (!response) {
+      setCurLiveAgent(0);
       return;
     }
     setCurLiveAgent(
-      null,
-      // `Running ${data?.data?.[0]?.type?.toLowerCase()} Analysis... \n ${statusMessages(selectedArtist?.name)[`${data.data[0].status}`]}`,
+      `Running ${getSocialPlatformByLink(response?.social?.profile_url)} Analysis... \n ${statusMessages[`${response?.status}`]} \n Created: ${new Date(response.updated_at).toLocaleString()}`,
     );
   }, [selectedArtist]);
 
   useEffect(() => {
-    const timer = setInterval(getLiveAgent, 2000);
+    getLiveAgent();
+    const timer = setInterval(getLiveAgent, 10000);
     return () => clearInterval(timer);
   }, [getLiveAgent]);
 
