22c055a3de354840d34716dc9342145925e771f7
dev <tech.eng.me@gmail.com>
2025-02-04 19:46:24 -0800

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -34,19 +34,10 @@ export async function POST(req: Request) {
     ],
     tools: {
       createArtist: tool({
-        description: `
-        IMPORTANT: Always call this tool for ANY question related to creating artist:
-        NOTE!!!: This feature will always run when prompted to create an artist, even if you don't get an artist name.
-        Do NOT attempt to answer questions on these topics without calling this tool first!!!
-    
-        Example questions that MUST trigger this tool:
-        - "Create a new artist."
-        - "Create an artist."
-        - "I wanna create a new artist."`,
+        description: 'Get the weather in a location',
         parameters: z.object({
           artist_name: z
             .string()
-            .optional()
             .describe("The name of the artist to be created."),
         }),
         execute: async ({ artist_name }) => ({
