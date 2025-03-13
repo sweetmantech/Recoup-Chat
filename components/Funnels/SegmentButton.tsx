c7edfe1165084bcb9a6693bc99bfce554b137a05
Sidney Swift <158200036+sidneyswift@users.noreply.github.com>
2025-03-13 15:16:42 -0400

    Fix mobile sidebar z-index bug by removing z-20 class from SegmentButton

--- components/Funnels/SegmentButton.tsx
+++ components/Funnels/SegmentButton.tsx
@@ -9,7 +9,7 @@ const SegmentButton = ({ segment, onGenerateReport }: SegmentButtonProps) => {
   return (
     <button
       onClick={() => onGenerateReport(segment.id, segment.name)}
-      className="bg-white border-2 border-black rounded-[10px] pl-5 pr-4 h-16 z-20 flex items-center gap-2 justify-between
+      className="bg-white border-2 border-black rounded-[10px] pl-5 pr-4 h-16 flex items-center gap-2 justify-between
         transition-all text-[15px] font-medium text-black hover:bg-black hover:text-white active:bg-white/80"
     >
       <div className="flex flex-col items-start">
