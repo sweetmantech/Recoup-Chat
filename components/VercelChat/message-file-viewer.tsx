062624db745aa2f6a825853ca7eaf1f1957fda6a
veerbal1 <veerbal.singh369@gmail.com>
2025-05-06 19:49:43 +0530

    Increased chat message file viewer width

--- components/VercelChat/message-file-viewer.tsx
+++ components/VercelChat/message-file-viewer.tsx
@@ -2,7 +2,7 @@ import { Attachment } from "@ai-sdk/ui-utils";
 
 const MessageFileViewer = ({ experimentalAttachment }: { experimentalAttachment: Attachment[] | undefined }) => {
     if (!experimentalAttachment || experimentalAttachment.length === 0) return null;
-    return <div className="max-w-xs flex gap-2 flex-wrap justify-end ml-auto">
+    return <div className="max-w-[21rem] flex gap-2 flex-wrap justify-end ml-auto">
         {experimentalAttachment?.map((attachment) => (
             <div key={attachment.url} className="w-40 h-40 rounded-md overflow-hidden shadow-md border">
                 {/* @eslint-disable-next-line @next/next/no-img-element */}
