42165720b9e6ac5fcb87e8cbfcda8c425c989b9d
sweetman.eth <sweetmantech@gmail.com>
2025-05-18 16:58:35 -0300

    Prefix arweave IDs with protocol

--- lib/txtGeneration.ts
+++ lib/txtGeneration.ts
@@ -46,10 +46,10 @@ export async function generateAndStoreTxtFile(
     // Continue and return the TXT even if Arweave upload fails
   }
 
-  // Create a collection on the blockchain
+  // Create a collection on the blockchain using the Arweave id
   const result = await createCollection({
     collectionName: contents,
-    uri: arweaveData?.url ?? "",
+    uri: arweaveData ? `ar://${arweaveData.id}` : "",
   });
   const transactionHash = result.transactionHash || null;
 
